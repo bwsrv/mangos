b5fab147c9f60a7bdeac0eb9e889f215e6170dee
mosst <makcim_Chaos@ukr.net>
2010-10-09 23:07:54 +0300

    [bws42] fixed visual bug with flying creatures.

--- src/game/Object.cpp
+++ src/game/Object.cpp
@@ -276,7 +276,7 @@ void Object::BuildMovementUpdate(ByteBuffer * data, uint16 updateFlags) const
                     if (!((Creature*)unit)->hasUnitState(UNIT_STAT_MOVING))
                     {
                         // (ok) possibly some "hover" mode
-                        unit->m_movementInfo.AddMovementFlag(MOVEFLAG_ROOT);
+                        // unit->m_movementInfo.AddMovementFlag(MOVEFLAG_ROOT);
                     }
                     else
                     {
