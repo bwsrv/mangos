6b65ba6a347e8a62b58f4525b47aad074ef871bc
mosst <makcim_Chaos@ukr.net>
2010-10-10 11:28:56 +0300

    [bws44] add spell_proc Improved Shadow Bolt and add stats Army of the Dead Ghoul.

--- bws_sql_updates/[bws00040]_mangos_pet_scaling_data.sql
+++
@@ -1,3 +0,0 @@
-UPDATE pet_scaling_data SET sta = 75, apbasescale = 428 WHERE creature_entry = 1 AND aura = 0;
-UPDATE pet_scaling_data SET sta = 15 WHERE creature_entry = 1 AND aura = 62758;
-UPDATE pet_scaling_data SET sta = 30 WHERE creature_entry = 1 AND aura = 62762;
