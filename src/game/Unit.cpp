0692d2b25295319fd474e2dc99c98422ef7ee1fa
emsy <emsy@gmail.com>
2010-10-14 08:59:21 +0300

    [bws63] Updating formula fir hit chances for melee and ranged abilities.

--- src/game/Unit.cpp
+++ src/game/Unit.cpp
@@ -3109,13 +3109,12 @@ float Unit::MeleeSpellMissChance(Unit *pVictim, WeaponAttackType attType, int32 
     // Calculate hit chance (more correct for chance mod)
     int32 HitChance;
 
-    // PvP - PvE melee chances
-    int32 lchance = pVictim->GetTypeId() == TYPEID_PLAYER ? 5 : 7;
+    // melee chances (same in PvP and PvE)
     int32 leveldif = pVictim->getLevelForTarget(this) - getLevelForTarget(pVictim);
-    if(leveldif < 3)
-        HitChance = 95 - leveldif;
+    if(skillDiff < -10)
+        HitChance = 95 + skillDiff * 0.1;
     else
-        HitChance = 93 - (leveldif - 2) * lchance;
+        HitChance = 94 + (skillDiff + 10) * 0.4;
 
     // Hit chance depends from victim auras
     if(attType == RANGED_ATTACK)
@@ -3135,9 +3134,6 @@ float Unit::MeleeSpellMissChance(Unit *pVictim, WeaponAttackType attType, int32 
         miss_chance -= m_modRangedHitChance;
     else
         miss_chance -= m_modMeleeHitChance;
-
-    // bonus from skills is 0.04%
-    miss_chance -= skillDiff * 0.04f;
 
     // Limit miss chance from 0 to 60%
     if (miss_chance < 0.0f)
