8777b1860e1b5919c62a014b2882b0cd99bdfd0b
mosst <Makcim_Chaos@ukr.net>
2010-10-22 21:46:31 +0300

    Revert "[bws78] Paladins Blessing stack fix."

--- src/game/SpellMgr.cpp
+++ src/game/SpellMgr.cpp
@@ -524,6 +524,7 @@ bool IsSingleFromSpellSpecificSpellRanksPerTarget(SpellSpecific spellSpec1,Spell
 {
     switch(spellSpec1)
     {
+        case SPELL_BLESSING:
         case SPELL_AURA:
         case SPELL_CURSE:
         case SPELL_ASPECT:
@@ -539,7 +540,6 @@ bool IsSingleFromSpellSpecificPerTarget(SpellSpecific spellSpec1,SpellSpecific s
 {
     switch(spellSpec1)
     {
-        case SPELL_BLESSING:
         case SPELL_SEAL:
         case SPELL_TRACKER:
         case SPELL_WARLOCK_ARMOR:
