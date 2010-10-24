fb797e21952d5a164cfc79ba79869013dadce71e
mosst <Makcim_Chaos@ukr.net>
2010-10-24 20:26:39 +0300

    [bws103] Add diminishing in Will of the Forsaken at use Insignia trinkets..
    Author: Me

--- src/game/SpellAuras.cpp
+++ src/game/SpellAuras.cpp
@@ -4841,7 +4841,7 @@ void Aura::HandleModMechanicImmunity(bool apply, bool /*Real*/)
         uint32 mechanic = 1 << (misc-1);
 
         //immune movement impairment and loss of control
-        if(GetId()==42292 || GetId()==59752 || GetId()==65547 || GetId()==53490)
+        if(GetId()==42292 || GetId()==59752 || GetId()==65547 || GetId()==53490 || GetId()==7744)
             mechanic=IMMUNE_TO_MOVEMENT_IMPAIRMENT_AND_LOSS_CONTROL_MASK;
 
         target->RemoveAurasAtMechanicImmunity(mechanic,GetId());
