c4617709d3919d73076ff6f5e9ed78f4b63976b2
mosst <Makcim_Chaos@Ukr.net>
2010-10-31 18:10:28 +0200

    [bws123] try fix Penance.

--- src/game/SpellAuras.cpp
+++ src/game/SpellAuras.cpp
@@ -2825,6 +2825,17 @@ void Aura::HandleAuraDummy(bool apply, bool Real)
         }
         case SPELLFAMILY_PRIEST:
         {
+            // Penance
+            if (GetSpellProto()->SpellIconID == 225 || GetSpellProto()->SpellIconID == 2818)
+            {
+                Unit* caster = GetCaster();
+                if (!caster || caster->GetTypeId() != TYPEID_PLAYER)
+                    return;
+
+                if (apply && target)
+                    ((Player*)caster)->SetSelectionGuid(target->GetGUID());
+                return;
+            }
             // Pain and Suffering
             if (GetSpellProto()->SpellIconID == 2874 && target->GetTypeId()==TYPEID_PLAYER)
             {
