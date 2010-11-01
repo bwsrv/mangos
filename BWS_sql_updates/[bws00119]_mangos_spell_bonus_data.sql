5f016db33fde72080d2621f39113d469b373fea1
mosst <Makcim_Chaos@Ukr.net>
2010-11-01 21:30:19 +0200

    Revert "[bws122] Change Ap bonuses for spell Death Coil."
    This reverts commit 8f3a67e9b05703b4f8ab75fd16fd09114603ef4e.

---
+++ BWS_sql_updates/[bws00119]_mangos_spell_bonus_data.sql
@@ -0,0 +1,7 @@
+-- Ancestral Awakening
+DELETE FROM spell_bonus_data WHERE entry = 52752;
+INSERT INTO spell_bonus_data (entry) VALUES (52752);
+
+-- Glyph of Prayer of Healing
+DELETE FROM spell_bonus_data WHERE entry = 56161;
+INSERT INTO spell_bonus_data (entry) VALUES (56161);
