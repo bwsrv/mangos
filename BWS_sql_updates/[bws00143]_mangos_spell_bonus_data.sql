a0b47655b2cdc88bd327c516a22cd31d72878b51
insider42 <insider42@gmail.com>
2010-11-07 22:39:37 +0200

    [bws143] Fixed spell damage and AP coeff. for spell 54158.

--- BWS_sql_updates/[bws00143]_mangos_spell_bonus_data.sql
+++ BWS_sql_updates/[bws00143]_mangos_spell_bonus_data.sql
@@ -9,3 +9,6 @@ INSERT INTO spell_bonus_data (entry) VALUES (56161);
 -- Arcane Blast
 DELETE FROM spell_bonus_data WHERE entry = 30451;
 INSERT INTO spell_bonus_data VALUES ('30451','0.714286','0.122','0','Mage - Arcane Blast');
+
+-- Judgement
+DELETE FROM spell_bonus_data WHERE entry = 54158;
