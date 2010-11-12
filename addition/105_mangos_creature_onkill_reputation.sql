d8193cb72e58ccbe32562e4f31a04fe92dd96522
insider42 <insider42@gmail.com>
2010-11-12 20:08:45 +0200

    [bws167] Fixed spell 56374.

--- addition/105_mangos_creature_onkill_reputation.sql
+++ addition/105_mangos_creature_onkill_reputation.sql
@@ -1,6 +1,3 @@
 ALTER TABLE `creature_onkill_reputation` 
-<<<<<<< HEAD
  ADD COLUMN `ChampioningAura` int(11) unsigned NOT NULL default '0' AFTER `TeamDependent`;
-=======
- ADD COLUMN `ChampioningAura` int(11) unsigned NOT NULL default '0' AFTER `TeamDependent`;
->>>>>>> 245a56bac457e51972bdf9e3dba28a0d1a3ed85e
+
