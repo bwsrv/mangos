260eb5c4bcc4b7be5aff0ef8d5e2e9cbfac1be26
mosst <Makcim_Chaos@Ukr.net>
2010-11-22 14:20:47 +0200

    [bws184] Crashfix SqlDelayThread::run.

--- src/shared/Database/SqlDelayThread.cpp
+++ src/shared/Database/SqlDelayThread.cpp
@@ -44,6 +44,7 @@ void SqlDelayThread::run()
         SqlOperation* s;
         while (m_sqlQueue.next(s))
         {
+            if (!s) continue;
             s->Execute(m_dbEngine);
             delete s;
         }
