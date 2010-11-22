2770ce893629af761ec0c734a66ffd6986784676
mosst <Makcim_Chaos@Ukr.net>
2010-11-22 14:25:20 +0200

    [bws185] Crashfix InstanceMap::Add(Player *player).

--- src/game/Map.cpp
+++ src/game/Map.cpp
@@ -1328,7 +1328,9 @@ bool InstanceMap::Add(Player *player)
                         GetInstanceSave()->GetMapId(), GetInstanceSave()->GetInstanceId(),
                         GetInstanceSave()->GetDifficulty(), GetInstanceSave()->GetPlayerCount(),
                         GetInstanceSave()->GetGroupCount(), GetInstanceSave()->CanReset());
-                    MANGOS_ASSERT(false);
+                  //MANGOS_ASSERT(false);
+                  if (!player->isGameMaster()) player->RepopAtGraveyard();
+                      return false;
                 }
             }
             else
@@ -1381,7 +1383,9 @@ bool InstanceMap::Add(Player *player)
                                 sLog.outError("GroupBind save players: %d, group count: %d", groupBind->save->GetPlayerCount(), groupBind->save->GetGroupCount());
                             else
                                 sLog.outError("GroupBind save NULL");
-                            MANGOS_ASSERT(false);
+                          //MANGOS_ASSERT(false);
+                          if (!player->isGameMaster()) player->TeleportToHomebind();
+                             return false;
                         }
                         // if the group/leader is permanently bound to the instance
                         // players also become permanently bound when they enter
@@ -1401,7 +1405,8 @@ bool InstanceMap::Add(Player *player)
                         player->BindToInstance(GetInstanceSave(), false);
                     else
                         // cannot jump to a different instance without resetting it
-                        MANGOS_ASSERT(playerBind->save == GetInstanceSave());
+                       //MANGOS_ASSERT(playerBind->save == GetInstanceSave());
+                       if (!player->isGameMaster()) player->RepopAtGraveyard();
                 }
             }
         }
