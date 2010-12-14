9e28514a5303b045b5d310b695e14d98b43ae162
mosst <Makcim_Chaos@Ukr.net>
2010-12-14 18:07:25 +0200

    Revert "[patch 112] Fixed invisibility for Arena Preparation. Patch provided by me"
    This reverts commit 958dd01486aac3e0db1ed86e880160952d96f260.
    
    Conflicts:
    
    	src/game/Unit.cpp

--- src/game/Unit.cpp
+++ src/game/Unit.cpp
@@ -8640,10 +8640,6 @@ bool Unit::isVisibleForOrDetect(Unit const* u, WorldObject const* viewPoint, boo
         invisible = false;
     }
 
-    // Arena preparation
-    if (HasAura(SPELL_ARENA_PREPARATION) && IsHostileTo(u))
-        invisible = true;
-
     // special cases for always overwrite invisibility/stealth
     if(invisible || m_Visibility == VISIBILITY_GROUP_STEALTH)
     {
