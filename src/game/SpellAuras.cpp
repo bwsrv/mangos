e94d7c6fb43ad506b9503533f8c4d3ae9d2dd1a7
PSZ <zavatski@gmail.com>
2011-01-08 12:42:18 +0200

    [bws223] The Might of Mograine must not affect scourge soldiers, must affect only players and DK bosses (by MaxXx2021)

--- src/game/SpellAuras.cpp
+++ src/game/SpellAuras.cpp
@@ -754,6 +754,10 @@ void AreaAura::Update(uint32 diff)
                 // flag for seelction is need apply aura to current iteration target
                 bool apply = true;
 
+                // The Might of Mograine must not affect scourge soldiers, must affect only players and DK bosses
+                if (GetSpellProto()->Id == 53642 && ((*tIter)->GetEntry() == 29186 || (*tIter)->GetEntry() == 29190 || (*tIter)->GetEntry() == 29219 || (*tIter)->GetEntry() == 29206))
+                    apply = false;
+
                 // we need ignore present caster self applied are auras sometime
                 // in cases if this only auras applied for spell effect
                 Unit::SpellAuraHolderBounds spair = (*tIter)->GetSpellAuraHolderBounds(GetId());
