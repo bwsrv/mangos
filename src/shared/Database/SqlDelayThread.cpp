258fee10f4ef34c1eb34d77fe109a98f3dcc50ca
maxim <makcim_Chaos@ukr.net>
2011-01-21 23:04:54 +0200

    Revert "[bws184] Crashfix SqlDelayThread::run."
    This reverts commit 260eb5c4bcc4b7be5aff0ef8d5e2e9cbfac1be26.

--- src/shared/Database/SqlDelayThread.cpp
+++ src/shared/Database/SqlDelayThread.cpp
@@ -52,7 +52,6 @@ void SqlDelayThread::run()
         SqlOperation* s = NULL;
         while (m_sqlQueue.next(s))
         {
-            if (!s) continue;
             s->Execute(m_dbEngine);
             delete s;
         }
