aee4ac218cd07c6e156cf8bc6a3301bfc3bd5675
mosst <Makcim_Chaos@Ukr.net>
2011-01-22 18:49:56 +0200

    Revert "implement Spell Consume"
    This reverts commit 594bfc6fa9b306f5e50105f21d600bdc34fc19ba.
    
    Conflicts:
    
    	src/game/SpellEffects.cpp

--- src/game/SpellEffects.cpp
+++ src/game/SpellEffects.cpp
@@ -6786,18 +6786,18 @@ void Spell::EffectScriptEffect(SpellEffectIndex eff_idx)
                     m_caster->CastSpell(m_caster, 43351, true);
                     return;
                 }
-                case 43375:
-                case 43972:                                // Mixing Blood for Quest 11306 
+				case 43375:
+				case 43972:		// Mixing Blood for Quest 11306 
                 {
-                    switch(urand(0, 2))
-                    {
-                        case 0 : m_caster->CastSpell(m_caster, 43378, true); break;
-                        case 1 : m_caster->CastSpell(m_caster, 43376, true); break;
-                        case 2 : m_caster->CastSpell(m_caster, 43377, true); break;
-                        case 3 : m_caster->CastSpell(m_caster, 43970, true); break;
-                    }
-                    break;
-                }
+					switch(urand(0, 2))
+					{
+						case 0 : m_caster->CastSpell(m_caster, 43378, true); break;
+						case 1 : m_caster->CastSpell(m_caster, 43376, true); break;
+						case 2 : m_caster->CastSpell(m_caster, 43377, true); break;
+						case 3 : m_caster->CastSpell(m_caster, 43970, true); break;
+					}
+					break;
+				}
                 case 44455:                                 // Character Script Effect Reverse Cast
                 {
                     if (!unitTarget || unitTarget->GetTypeId() != TYPEID_UNIT)
@@ -7143,11 +7143,6 @@ void Spell::EffectScriptEffect(SpellEffectIndex eff_idx)
                     unitTarget->CastSpell(unitTarget, unitTarget->getGender() == GENDER_MALE ? 38080 : 38081, true);
                     // Shadowy Disguise
                     unitTarget->CastSpell(unitTarget, 32756, true);
-                    return;
-                }
-                case 49380:                                 // Consume: Spell of Trollgore nonhero
-                {
-                    m_caster->CastSpell(m_caster,49381,true);
                     return;
                 }
                 case 50217:                                 // The Cleansing: Script Effect Player Cast Mirror Image
@@ -7546,6 +7541,7 @@ void Spell::EffectScriptEffect(SpellEffectIndex eff_idx)
                         unitTarget->CastSpell(unitTarget, 59314, true);
 
                     return;
+<<<<<<< HEAD
                 }  
                 case 59910:                                 // Summon Minions
                 {
@@ -7565,6 +7561,9 @@ void Spell::EffectScriptEffect(SpellEffectIndex eff_idx)
                     return;
                 }
                                                             // random spell learn instead placeholder
+=======
+                }                                           // random spell learn instead placeholder
+>>>>>>> parent of 594bfc6... implement Spell Consume
                 case 60893:                                 // Northrend Alchemy Research
                 case 61177:                                 // Northrend Inscription Research
                 case 61288:                                 // Minor Inscription Research
