8f8206fffefe65cfdd182d3b8350ee111d949a51
PSZ <zavatski@gmail.com>
2011-02-28 17:11:51 +0200

    [bws267] Warrior's Demoralizing Shout must break stealth

--- src/game/Spell.cpp
+++ src/game/Spell.cpp
@@ -1331,7 +1331,6 @@ void Spell::DoSpellHitOnUnit(Unit *unit, const uint32 effectMask)
             if (!(m_spellInfo->AttributesEx & SPELL_ATTR_EX_NOT_BREAK_STEALTH) &&
                 m_spellInfo->Id != 51690 && m_spellInfo->Id != 53055 && m_spellInfo->Id != 3600 &&
                 m_spellInfo->Id != 44416 && m_spellInfo->SpellIconID != 1954 && m_spellInfo->SpellIconID != 2267 &&
-                !(m_spellInfo->SpellFamilyName == SPELLFAMILY_WARRIOR && m_spellInfo->SpellIconID == 282) && // Warrior's Demoralizing Shout
                 !(m_spellInfo->SpellFamilyName == SPELLFAMILY_WARLOCK && m_spellInfo->SpellIconID == 9) && // Warlock's Voidwalker's Suffering
                 m_spellInfo->Id != 39897 && m_spellInfo->Id != 32592 && m_spellInfo->Id != 32375 && m_spellInfo->Id != 1725 &&
                 m_spellInfo->Id != 1038 && !(m_spellInfo->SpellFamilyFlags2 & UI64LIT(0x00000100)) && m_spellInfo->SpellFamilyFlags != SPELLFAMILYFLAG_ROGUE_SAP)
