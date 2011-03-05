b044b126e2213a1b7358ec8f50d760409143ebfa
Janu <jaed1@gmx.net>
2011-03-05 16:27:46 +0100

    [IOV94] added check for spell Prey on the Weak
    without this check, spell procs without checking HP
    this is because it is not clear if mob or player is the target
    
    Signed-off-by: Janu <jaed1@gmx.net>

--- src/game/SpellAuras.cpp
+++ src/game/SpellAuras.cpp
@@ -8543,6 +8543,9 @@ void Aura::PeriodicDummyTick()
             // Prey on the Weak
             if (spell->SpellIconID == 2983)
             {
+                if (target->GetTypeId() != TYPEID_PLAYER)
+                    return;
+
                 Unit *victim = target->getVictim();
                 if (victim && (target->GetHealth() * 100 / target->GetMaxHealth() > victim->GetHealth() * 100 / victim->GetMaxHealth()))
                 {
