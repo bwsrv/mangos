53dcf4aca289cfb436cc8a2632622865031b6343
Berni <andislittlefighter2@arcor.de>
2011-03-11 01:09:01 +0100

    [IOV129] glow vision shouldn't be active during arena preparation
    Signed-off-by: Janu <jaed1@gmx.net>

--- src/game/SpellAuras.cpp
+++ src/game/SpellAuras.cpp
@@ -4874,9 +4874,13 @@ void Aura::HandleInvisibility(bool apply, bool Real)
 
         if (Real && target->GetTypeId()==TYPEID_PLAYER)
         {
-            // apply glow vision
-            target->SetByteFlag(PLAYER_FIELD_BYTES2, 3, PLAYER_FIELD_BYTE2_INVISIBILITY_GLOW);
-
+            Player * pl = (Player*)target;
+            // do not apply during arena preparation
+            if (!pl->GetMap()->IsBattleArena() || (!pl->GetBattleGround() || !(pl->GetBattleGround()->GetStatus() == STATUS_WAIT_JOIN)))
+            {
+                // apply glow vision
+                target->SetByteFlag(PLAYER_FIELD_BYTES2, 3, PLAYER_FIELD_BYTE2_INVISIBILITY_GLOW);
+            }
         }
 
         // apply only if not in GM invisibility and not stealth
