e0a285e0d179773e5b096f5ec3ae16fe19ae3082
Ankso <administrador.ankso@reinodelaoscuridad.com>
2011-03-11 12:52:58 +0100

    AFK and DND messages do not include language field any more. Parse commands when they are sent to channels. Allows to use commands when dead e.g. to .revive. By Chameleon

--- src/game/ChatHandler.cpp
+++ src/game/ChatHandler.cpp
@@ -64,6 +64,48 @@ void WorldSession::HandleMessagechatOpcode( WorldPacket & recv_data )
     uint32 lang;
 
     recv_data >> type;
+    // no language for AFK and DND messages
+    if(type == CHAT_MSG_AFK)
+    {
+        
+        std::string msg;
+        recv_data >> msg;
+
+        if ((msg.empty() || !_player->isAFK()) && !_player->isInCombat())
+        {
+            if (!_player->isAFK())
+            {
+                if (msg.empty())
+                    msg  = GetMangosString(LANG_PLAYER_AFK_DEFAULT);
+                _player->afkMsg = msg;
+            }
+
+            _player->ToggleAFK();
+            if (_player->isAFK() && _player->isDND())
+                _player->ToggleDND();
+        }
+        return;
+    }
+    else if(type == CHAT_MSG_DND)
+    {
+        std::string msg;
+        recv_data >> msg;
+
+        if (msg.empty() || !_player->isDND())
+        {
+            if (!_player->isDND())
+            {
+                if (msg.empty())
+                    msg  = GetMangosString(LANG_PLAYER_DND_DEFAULT);
+                _player->dndMsg = msg;
+            }
+
+            _player->ToggleDND();
+            if (_player->isDND() && _player->isAFK())
+                _player->ToggleAFK();
+        }
+        return;
+    }
     recv_data >> lang;
 
     if(type >= MAX_CHAT_MSG_TYPE)
@@ -461,6 +503,12 @@ void WorldSession::HandleMessagechatOpcode( WorldPacket & recv_data )
             recv_data >> channel;
             recv_data >> msg;
 
+            if (msg.empty())
+                break;
+
+            if (ChatHandler(this).ParseCommands(msg.c_str()) > 0)
+                break;
+
             if (!processChatmessageFurtherAfterSecurityChecks(msg, lang))
                 return;
 
@@ -472,49 +520,6 @@ void WorldSession::HandleMessagechatOpcode( WorldPacket & recv_data )
             if(ChannelMgr* cMgr = channelMgr(_player->GetTeam()))
                 if(Channel *chn = cMgr->GetChannel(channel, _player))
                     chn->Say(_player->GetGUID(), msg.c_str(), lang);
-        } break;
-
-        case CHAT_MSG_AFK:
-        {
-            std::string msg;
-            recv_data >> msg;
-
-            if (!_player->isInCombat())
-            {
-                if (!msg.empty() || !_player->isAFK())
-                {
-                    if (msg.empty())
-                        _player->afkMsg = GetMangosString(LANG_PLAYER_AFK_DEFAULT);
-                    else
-                        _player->afkMsg = msg;
-                }
-                if (msg.empty() || !_player->isAFK())
-                {
-                    _player->ToggleAFK();
-                    if (_player->isAFK() && _player->isDND())
-                        _player->ToggleDND();
-                }
-            }
-        } break;
-
-        case CHAT_MSG_DND:
-        {
-            std::string msg;
-            recv_data >> msg;
-
-            if (!msg.empty() || !_player->isDND())
-            {
-                if (msg.empty())
-                    _player->dndMsg = GetMangosString(LANG_PLAYER_DND_DEFAULT);
-                else
-                    _player->dndMsg = msg;
-            }
-            if (msg.empty() || !_player->isDND())
-            {
-                _player->ToggleDND();
-                if (_player->isDND() && _player->isAFK())
-                    _player->ToggleAFK();
-            }
         } break;
 
         default:
