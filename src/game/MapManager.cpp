8aede2c8254337eb24b650856dca1f66e9ff456c
PSZ <zavatski@gmail.com>
2011-03-13 13:09:53 +0200

    [bws285] Restored debug log record when player tries to enter instance when encounter is in progress

--- src/game/MapManager.cpp
+++ src/game/MapManager.cpp
@@ -237,6 +237,7 @@ bool MapManager::CanPlayerEnter(uint32 mapid, Player* player)
             InstanceData* instance_data = ((DungeonMap*)CreateMap(mapid, player))->GetInstanceData();
             if (instance_data && instance_data->IsEncounterInProgress())
             {
+                DEBUG_LOG("MAP: Player '%s' can't enter instance '%s' while an encounter is in progress.", player->GetName(), mapName);
                 player->SendTransferAborted(mapid, TRANSFER_ABORT_ZONE_IN_COMBAT);
                 return false;
             }
