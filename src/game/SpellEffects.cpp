2b531921004b66804bcd9e8f70a688446a678ed2
PSZ <zavatski@gmail.com>
2011-03-18 13:55:27 +0200

    [bws292] Improved spell Gift of the Harvester (related to DK start quest The Gift That Keeps On Giving).

--- src/game/SpellEffects.cpp
+++ src/game/SpellEffects.cpp
@@ -272,9 +272,6 @@ void Spell::EffectInstaKill(SpellEffectIndex /*eff_idx*/)
     if (!unitTarget || !unitTarget->isAlive())
         return;
 
-    if(m_spellInfo->Id==52479 && unitTarget->GetTypeId()==TYPEID_PLAYER)
-        return;
-
     if (m_caster == unitTarget)                              // prevent interrupt message
         finish();
 
@@ -7590,12 +7587,13 @@ void Spell::EffectScriptEffect(SpellEffectIndex eff_idx)
                     unitTarget->CastSpell(unitTarget, m_spellInfo->EffectBasePoints[eff_idx]+1, true);
                     break;
                 }
-                case 52479:                                 // The Gift That Keeps On Giving - trigger Scarlet Miner Ghoul Transform
+                case 52479:                                 // The Gift That Keeps On Giving
                 {
-                    if (m_caster->GetTypeId() != TYPEID_PLAYER || unitTarget->GetTypeId() == TYPEID_PLAYER)
+                    if (!m_caster || !unitTarget)
                         return;
 
-                    m_caster->CastSpell(m_caster, m_spellInfo->EffectBasePoints[eff_idx]+1, true);
+                    m_caster->CastSpell(m_caster, roll_chance_i(75) ? 52505 : m_spellInfo->EffectBasePoints[eff_idx]+1, true);
+                    ((Creature*)unitTarget)->ForcedDespawn();
                     break;
                 }
                 case 52694:                                 // Recall Eye of Acherus
