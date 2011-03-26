a0a1e1364b36d8e39c947d3180de1a9e323e18e0
Janu <jaed1@gmx.net>
2011-03-26 18:41:15 +0100

    [IOV169] mana adjustment for Replenishment, thx Dunbaldor
    Signed-off-by: Janu <jaed1@gmx.net>

--- src/game/SpellAuras.cpp
+++ src/game/SpellAuras.cpp
@@ -5605,8 +5605,8 @@ void Aura::HandlePeriodicEnergize(bool apply, bool Real)
             case 48391:                                     // Owlkin Frenzy (2% from base) mana
                 m_modifier.m_amount = target->GetCreateMana() * GetBasePoints() / 100;
                 break;
-            case 57669:                                     // Replenishment (1% from max)
-                m_modifier.m_amount = target->GetMaxPower(POWER_MANA) * GetBasePoints() / 100;
+            case 57669:                                     // Replenishment (0,2% from max)
+                m_modifier.m_amount = target->GetMaxPower(POWER_MANA) * 2 / 1000;
                 break;
             case 61782:                                     // Infinite Replenishment (0,25% from max)
                 m_modifier.m_amount = target->GetMaxPower(POWER_MANA) * 25 / 10000;
