de9d97ec4ae44916503d25fb7ea766c0aec55781
/dev/rsa <kris13@bk.ru>
2011-04-13 21:15:26 +0400

    event handling for GO type 33

--- src/game/GameObject.cpp
+++ src/game/GameObject.cpp
@@ -1718,6 +1718,7 @@ void GameObject::DamageTaken(Unit* pDoneBy, uint32 damage)
             RemoveFlag(GAMEOBJECT_FLAGS, GO_FLAG_DAMAGED);
             SetFlag(GAMEOBJECT_FLAGS, GO_FLAG_DESTROYED);
             SetUInt32Value(GAMEOBJECT_DISPLAYID, m_goInfo->destructibleBuilding.destroyedDisplayId);
+            GetMap()->ScriptsStart(sEventScripts, m_goInfo->destructibleBuilding.destroyedEvent, pDoneBy, this);
         }
     }
     else                                            // from intact to damaged
@@ -1726,6 +1727,7 @@ void GameObject::DamageTaken(Unit* pDoneBy, uint32 damage)
         {
             SetFlag(GAMEOBJECT_FLAGS, GO_FLAG_DAMAGED);
             SetUInt32Value(GAMEOBJECT_DISPLAYID, m_goInfo->destructibleBuilding.damagedDisplayId);
+            GetMap()->ScriptsStart(sEventScripts, m_goInfo->destructibleBuilding.damageEvent, pDoneBy, this);
             // if we have a "dead" display we can "kill" the building after its damaged
             if (m_goInfo->destructibleBuilding.destroyedDisplayId)
             {
@@ -1749,6 +1751,7 @@ void GameObject::Rebuild(Unit* pWho)
     RemoveFlag(GAMEOBJECT_FLAGS, GO_FLAG_DAMAGED | GO_FLAG_DESTROYED);
     SetUInt32Value(GAMEOBJECT_DISPLAYID, m_goInfo->displayId);
     m_health = GetMaxHealth();
+    GetMap()->ScriptsStart(sEventScripts, m_goInfo->destructibleBuilding.rebuildingEvent, pWho, this);
 
     SetGoAnimProgress(255);
 }
