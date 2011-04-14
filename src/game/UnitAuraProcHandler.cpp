6837c1d4ef3efb1a08eab5f248e327368d5ae3c2
Selector <limurchick@gmail.com>
2011-04-14 20:36:37 +0400

    Priest T10 Healer 2P Bonus

--- src/game/UnitAuraProcHandler.cpp
+++ src/game/UnitAuraProcHandler.cpp
@@ -1714,6 +1714,13 @@ SpellAuraProcResult Unit::HandleDummyAuraProc(Unit *pVictim, uint32 damage, Aura
                     triggered_spell_id = 56161;
                     break;
                 }
+                // Priest T10 Healer 2P Bonus
+                case 70770:
+                {
+                    basepoints[0] = triggerAmount*damage/100/3;
+                    triggered_spell_id = 70772;
+                    break;
+                }
             }
             break;
         }
