20b796fbb0f97320929eaff3bded10e300840929
/dev/rsa <kris13@bk.ru>
2011-04-19 18:58:38 +0400

    Revert "try to remove crush in MapPersistentState::ClearRespawnTimes. TESTING!"
    This reverts commit b2f74c53568d86bbbe8b2235653049240abaa5f2.

--- src/game/MapPersistentStateMgr.cpp
+++ src/game/MapPersistentStateMgr.cpp
@@ -121,7 +121,6 @@ void MapPersistentState::SaveGORespawnTime(uint32 loguid, time_t t)
 
 void MapPersistentState::SetCreatureRespawnTime( uint32 loguid, time_t t )
 {
-    sMapPersistentStateMgr.m_persistentStateLock.acquire();
     if (t > sWorld.GetGameTime())
         m_creatureRespawnTimes[loguid] = t;
     else
@@ -129,12 +128,10 @@ void MapPersistentState::SetCreatureRespawnTime( uint32 loguid, time_t t )
         m_creatureRespawnTimes.erase(loguid);
         UnloadIfEmpty();
     }
-    sMapPersistentStateMgr.m_persistentStateLock.release();
 }
 
 void MapPersistentState::SetGORespawnTime( uint32 loguid, time_t t )
 {
-    sMapPersistentStateMgr.m_persistentStateLock.acquire();
     if (t > sWorld.GetGameTime())
         m_goRespawnTimes[loguid] = t;
     else
@@ -142,17 +139,14 @@ void MapPersistentState::SetGORespawnTime( uint32 loguid, time_t t )
         m_goRespawnTimes.erase(loguid);
         UnloadIfEmpty();
     }
-    sMapPersistentStateMgr.m_persistentStateLock.release();
 }
 
 void MapPersistentState::ClearRespawnTimes()
 {
-    sMapPersistentStateMgr.m_persistentStateLock.acquire();
     m_goRespawnTimes.clear();
     m_creatureRespawnTimes.clear();
 
     UnloadIfEmpty();
-    sMapPersistentStateMgr.m_persistentStateLock.release();
 }
 
 void MapPersistentState::AddCreatureToGrid( uint32 guid, CreatureData const* data )
