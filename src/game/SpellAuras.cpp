c6b57b3b2e5db95d80d40ff6badebadfcc811cd3
/dev/rsa <kris13@bk.ru>
2011-04-22 13:42:34 +0400

    fix typo

--- src/game/SpellAuras.cpp
+++ src/game/SpellAuras.cpp
@@ -6151,7 +6151,7 @@ void Aura::HandleAuraModIncreaseEnergy(bool apply, bool Real)
 
     if(int32(powerType) != m_modifier.m_miscvalue)
     {
-        DEBUG_LOG("HandleAuraModIncreaseEnergy: unit %u change energy %u but current type %", target->GetObjectGuid().GetCounter(), m_modifier.m_miscvalue, powerType);
+        DEBUG_LOG("HandleAuraModIncreaseEnergy: unit %u change energy %u but current type %u", target->GetObjectGuid().GetCounter(), m_modifier.m_miscvalue, powerType);
         powerType = Powers(m_modifier.m_miscvalue);
     }
 
@@ -6184,7 +6184,7 @@ void Aura::HandleAuraModIncreaseEnergyPercent(bool apply, bool /*Real*/)
 
     if(int32(powerType) != m_modifier.m_miscvalue)
     {
-        DEBUG_LOG("HandleAuraModIncreaseEnergy: unit %u change energy %u but current type %", target->GetObjectGuid().GetCounter(), m_modifier.m_miscvalue, powerType);
+        DEBUG_LOG("HandleAuraModIncreaseEnergy: unit %u change energy %u but current type %u", target->GetObjectGuid().GetCounter(), m_modifier.m_miscvalue, powerType);
         powerType = Powers(m_modifier.m_miscvalue);
     }
 
