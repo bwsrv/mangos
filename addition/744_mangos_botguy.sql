31e6296a99509c63d6d00a6bdca36d0cc40dcc28
/dev/rsa <kris13@bk.ru>
2011-04-26 12:40:52 +0400

    sql definition for playerbot

--- addition/744_mangos_botguy.sql
+++ addition/744_mangos_botguy.sql
@@ -1,1 +1,5 @@
+--
+INSERT INTO `command` (`name`,`security`,`help`) VALUES 
+('bot',0,'Syntax:\r.bot add BOTNAME (add character to world)\r.bot remove BOTNAME (remove character from world)\r.bot co|combatorder BOTNAME COMBATORDER [TARGET]');
+--
 INSERT INTO `gossip_menu_option` VALUES('0','16','0','GOSSIP_OPTION_BOT','18','1','0','0','0','0','0',NULL,'0','0','0','0','0','0','0','0','0');
