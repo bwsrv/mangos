e8da198d492bdaaf185e8f44dfe8930613378632
/dev/rsa <kris13@bk.ru>
2011-04-27 18:59:28 +0400

    avoid strange crush in IsFriendly()

--- src/game/Unit.cpp
+++ src/game/Unit.cpp
@@ -5802,6 +5802,9 @@ bool Unit::IsHostileTo(Unit const* unit) const
 
 bool Unit::IsFriendlyTo(Unit const* unit) const
 {
+    if (!unit)
+        return true;
+
     // always friendly to self
     if(unit==this)
         return true;
