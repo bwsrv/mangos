79cf260f2e050113056e2206b2f4dc60053f4cc2
Ancient <anubis@abwesend.de>
2011-05-01 19:09:49 +0200

    [IOV252] support for some Children's Week achievements
    thx to TC
    
    Signed-off-by: Janu <jaed1@gmx.net>

--- src/game/DBCEnums.h
+++ src/game/DBCEnums.h
@@ -213,6 +213,11 @@ enum AchievementCriteriaTypes
 
 #define ACHIEVEMENT_CRITERIA_TYPE_TOTAL 124
 
+enum AchievementCategory
+{
+    CATEGORY_CHILDRENS_WEEK     = 163,
+};
+
 enum AreaFlags
 {
     AREA_FLAG_SNOW                  = 0x00000001,           // snow (only Dun Morogh, Naxxramas, Razorfen Downs and Winterspring)
