c84d2eaf7cb9b664d42367196d5693c71640b2cd
mosst <Makcim_Chaos@Ukr.net>
2011-05-09 00:01:00 +0300

    Some typo in sql.

--- sql_mr/mr00001_mangos_spell_bonus_data.sql
+++ sql_mr/mr00001_mangos_spell_bonus_data.sql
@@ -8,7 +8,7 @@ DELETE FROM spell_bonus_data WHERE entry = 56161;
 INSERT INTO spell_bonus_data (entry) VALUES (56161);
 -- Arcane Blast
 DELETE FROM spell_bonus_data WHERE entry = 30451;
-INSERT INTO spell_bonus_data VALUES ('30451','0.714286','0.122','0','Mage - Arcane Blast');
+INSERT INTO spell_bonus_data VALUES (30451, 0.714286, 0.122, 0, 0, 'Mage - Arcane Blast');
 -- Judgement
 DELETE FROM spell_bonus_data WHERE entry = 54158;
 -- Gargoyle Strike
