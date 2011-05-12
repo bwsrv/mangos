298624e012fc684aa5beff0e100e77d1dd6f1f8a
mosst <Makcim_Chaos@Ukr.net>
2011-05-12 19:54:44 +0300

    Revert "[bws347] Implement enable/disable PvP on Area."
    This reverts commit 33eeaa64c44f61e346e3a4596c9352da2b257b3f.

--- BWS_sql_updates/[bws00347]_mangos_area_flags.sql
+++
@@ -1,6 +0,0 @@
-DROP TABLE IF EXISTS area_flags;
-CREATE TABLE area_flags (
-  area_id mediumint(6) NOT NULL DEFAULT '0',
-  area_flag int(2) unsigned NOT NULL DEFAULT '0',
-  PRIMARY KEY  (area_id)
-) ENGINE = MyISAM DEFAULT CHARSET = utf8;
