65255453f0074bc6fc189d7df7abb4941826b7fa
mosst <Makcim_Chaos@Ukr.net>
2011-05-20 12:48:02 +0300

    Revert "[bws189] Fixed spell 54732 has 67% chance of success."
    This reverts commit ab73928b5363ba06b7c6210a7bbf7a0c2ea7ef78.

--- src/game/SpellEffects.cpp
+++ src/game/SpellEffects.cpp
@@ -9786,11 +9786,6 @@ void Spell::EffectResurrect(SpellEffectIndex /*eff_idx*/)
                 return;
             }
             break;
-        // Defibrillate (Gnomish Army Knife) has 67% chance of success
-        case 54732:
-            if (roll_chance_i(33))
-                return;
-            break;
         default:
             break;
     }
