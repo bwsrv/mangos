a7716b70a6a95fdea5655b6ec51af7a8de16e395
mosst <Makcim_Chaos@Ukr.net>
2011-05-24 16:35:43 +0300

    [bws357] Cleanup and Update SQL files.

--- BWS_sql_updates/[bws00021]_mangos_autobroadcast.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS `autobroadcast` (
-  `id` int(11) NOT NULL auto_increment,
-  `text` longtext NOT NULL,
-  `next` int(11) NOT NULL,
-  PRIMARY KEY  (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8;
-
-DELETE FROM `mangos_string` WHERE `entry` = 1300;
-INSERT INTO `mangos_string` VALUES (1300, '|cffffcc00[Server]: |cff00ff00%s|r', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
