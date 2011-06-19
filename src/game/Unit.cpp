0d33c1ffe62fc8d945f3861f95b3e18fcbf1dc7e
michalpolko <michalpolko@o2.pl>
2011-06-19 10:40:50 +0200

    [as37] Better holder stacking rules.

--- src/game/Unit.cpp
+++ src/game/Unit.cpp
@@ -4235,13 +4235,23 @@ bool Unit::AddSpellAuraHolder(SpellAuraHolder *holder)
                 if (!foundHolder->m_auras[i] || !holder->m_auras[i])
                     continue;
 
-                // one holder removes another if it has no DoT/HoT effect
+                // m_auraname can be modified to SPELL_AURA_NONE for area auras, use original
                 AuraType aurNameReal = AuraType(aurSpellInfo->EffectApplyAuraName[i]);
 
-                if (aurNameReal == SPELL_AURA_PERIODIC_HEAL || aurNameReal == SPELL_AURA_PERIODIC_DAMAGE)
+                switch(aurNameReal)
                 {
-                    bRemove = false;
-                    break;
+                    // DoT/HoT/etc
+                    case SPELL_AURA_DUMMY:                  // allow stack
+                    case SPELL_AURA_PERIODIC_DAMAGE:
+                    case SPELL_AURA_PERIODIC_DAMAGE_PERCENT:
+                    case SPELL_AURA_PERIODIC_LEECH:
+                    case SPELL_AURA_PERIODIC_HEAL:
+                    case SPELL_AURA_OBS_MOD_HEALTH:
+                    case SPELL_AURA_PERIODIC_MANA_LEECH:
+                    case SPELL_AURA_OBS_MOD_MANA:
+                    case SPELL_AURA_POWER_BURN_MANA:
+                        bRemove = false;
+                        break;
                 }
             }
 
