e5ec591cef4a2eb42db449f6d417d65846d78f82
mosst <Makcim_Chaos@Ukr.net>
2011-06-22 23:33:43 +0300

    [mr244] fixed spell Unstable Ooze Explosion (Icecrown Citadel, Big Ooze).

--- src/game/SpellAuras.cpp
+++ src/game/SpellAuras.cpp
@@ -5466,6 +5466,11 @@ void Aura::HandlePeriodicTriggerSpell(bool apply, bool /*Real*/)
                 }
 
                 return;
+            case 71441:                                     // Unstable Ooze Explosion (Icecrown Citadel encounter)
+                if (m_removeMode == AURA_REMOVE_BY_EXPIRE)
+                    target->CastSpell(target, 67375, true, NULL, this);
+
+                return;
             default:
                 break;
         }
--- src/shared/revision_R2.h
+++ src/shared/revision_R2.h
@@ -1,4 +1,4 @@
 #ifndef __REVISION_R2_H__
 #define __REVISION_R2_H__
- #define REVISION_R2 "243"
+ #define REVISION_R2 "244"
 #endif // __REVISION_R2_H__
