581adf973b14425bb3ef9e10c323cbdcb19fc3d0
mosst <Makcim_Chaos@ukr.net>
2011-07-05 10:32:42 +0300

    [mr339] Fixed spell Infinite Replenishment.

--- src/game/SpellAuras.cpp
+++ src/game/SpellAuras.cpp
@@ -5551,8 +5551,10 @@ void Aura::HandlePeriodicEnergize(bool apply, bool Real)
                 m_modifier.m_amount = target->GetCreateMana() * 2 / 100;
                 break;
             case 57669:                                     // Replenishment (0.2% from max)
-            case 61782:                                     // Infinite Replenishment
                 m_modifier.m_amount = target->GetMaxPower(POWER_MANA) * 2 / 1000;
+                break;
+            case 61782:                                     // Infinite Replenishment (0.25% from max)
+                m_modifier.m_amount = target->GetMaxPower(POWER_MANA) * 25 / 10000;
                 break;
             default:
                 break;
