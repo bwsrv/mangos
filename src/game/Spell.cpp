c746c10759ee79eb242dc3587220b91e00adb7e5
mosst <Makcim_Chaos@ukr.net>
2011-07-06 09:56:07 +0300

    [IOV353] Check LoS for spells cast on ground (like Mass Dispel)
    check if spell can be cast
    affected targets are not included in this commit
    
    Signed-off-by: Janu <jaed1@gmx.net>
    
    Conflicts:
    
    	src/game/Spell.cpp

--- src/game/Spell.cpp
+++ src/game/Spell.cpp
@@ -6173,6 +6173,12 @@ SpellCastResult Spell::CheckCast(bool strict)
 
     if (m_spellInfo->Id == 781 && !m_caster->isInCombat()) //Disengage
          return SPELL_FAILED_CANT_DO_THAT_RIGHT_NOW;
+    // check LOS for ground targeted spells
+    if (!m_targets.getUnitTarget() && !m_targets.getGOTarget() && !m_targets.getItemTarget())
+    {
+        if (m_targets.m_destX && m_targets.m_destY && m_targets.m_destZ && !m_caster->IsWithinLOS(m_targets.m_destX, m_targets.m_destY, m_targets.m_destZ))
+            return SPELL_FAILED_LINE_OF_SIGHT;
+    }
     // all ok
     return SPELL_CAST_OK;
 }
