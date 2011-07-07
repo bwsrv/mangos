a6c1965114d5bdff35c8fe90bde7a1e6398be81e
mosst <Makcim_Chaos@ukr.net>
2011-07-07 18:20:05 +0300

    Revert "[mr363] Fixed spell Tickbird Signal to Fall."
    This reverts commit 0b9a48618887b27753f5ea0957c30fd945435c1b.
    
    Conflicts:
    
    	src/shared/revision_R2.h

--- src/game/SpellEffects.cpp
+++ src/game/SpellEffects.cpp
@@ -2262,14 +2262,6 @@ void Spell::EffectDummy(SpellEffectIndex eff_idx)
                     m_caster->CastSpell(unitTarget, 50770, true);
                     return;
                 }
-                case 51369:                                 // Tickbird Signal to Fall
-                {
-                    if (!unitTarget)
-                        return;
-
-                    unitTarget->DealDamage(unitTarget, unitTarget->GetMaxHealth(), NULL, DIRECT_DAMAGE, SPELL_SCHOOL_MASK_NORMAL, NULL, false);
-                    return;
-                }
                 case 51420:                                 // Digging for Treasure Ping
                 {
                     if (!unitTarget || unitTarget->GetTypeId() != TYPEID_UNIT)
