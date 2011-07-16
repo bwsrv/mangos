61c99881105c0e5ad86947d4cd1a2cdfb9957b43
michalpolko <michalpolko@o2.pl>
2011-07-16 16:25:48 +0200

    [mr424] Check LoS for AoE spells with destination target selection (like Mass Dispel).

--- src/game/Spell.cpp
+++ src/game/Spell.cpp
@@ -6152,6 +6152,13 @@ SpellCastResult Spell::CheckCast(bool strict)
         }
     }
 
+    // check LOS for ground targeted spells
+    if (!m_targets.getUnitTarget() && !m_targets.getGOTarget() && !m_targets.getItemTarget())
+    {
+        if (m_targets.m_destX && m_targets.m_destY && m_targets.m_destZ && !m_caster->IsWithinLOS(m_targets.m_destX, m_targets.m_destY, m_targets.m_destZ))
+            return SPELL_FAILED_LINE_OF_SIGHT;
+    }
+
     // all ok
     return SPELL_CAST_OK;
 }
