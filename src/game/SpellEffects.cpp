8806bb8a12e2d0a2c655d00482594b21c6733097
/dev/rsa <kris13@bk.ru>
2011-07-17 22:19:42 +0400

    [mr479] revert knockback immunity for worldbosses - wrong

--- src/game/SpellEffects.cpp
+++ src/game/SpellEffects.cpp
@@ -10273,11 +10273,6 @@ void Spell::EffectKnockBack(SpellEffectIndex eff_idx)
     if (unitTarget->hasUnitState(UNIT_STAT_ROOT))
         return;
 
-    // Can't knockback world bosses (don't know any exceptions)
-    if (unitTarget->GetTypeId() != TYPEID_PLAYER)
-        if (((Creature*)unitTarget)->IsWorldBoss())
-            return;
-
     // Typhoon
     if (m_spellInfo->SpellFamilyName == SPELLFAMILY_DRUID && m_spellInfo->SpellFamilyFlags.test<CF_DRUID_TYPHOON>())
         if (m_caster->HasAura(62135)) // Glyph of Typhoon
