91dbd2b11d17b652377fb94f003adf28b99261fa
/dev/rsa <kris13@bk.ru>
2011-07-23 01:11:41 +0400

    [mr518] P;ayerBot: fix problem with moving after 11720. Thx to SilverIce.

--- src/game/playerbot/PlayerbotAI.cpp
+++ src/game/playerbot/PlayerbotAI.cpp
@@ -2649,10 +2649,9 @@ void PlayerbotAI::UpdateAI(const uint32 p_time)
     if (movementType == CHASE_MOTION_TYPE || movementType == POINT_MOTION_TYPE)
     {
         float x, y, z;
-        m_bot->GetMotionMaster()->GetDestination(x, y, z);
-        if (x != m_destX || y != m_destY || z != m_destZ)
+        if (m_bot->GetMotionMaster()->GetDestination(x, y, z))
         {
-            m_bot->MonsterMoveWithSpeed(x, y, z, 28);
+            m_bot->MonsterMoveWithSpeed(x, y, z, DEFAULT_WALK_SPEED);
             m_destX = x;
             m_destY = y;
             m_destZ = z;
@@ -3756,6 +3755,7 @@ bool PlayerbotAI::FollowCheckTeleport(WorldObject &obj)
 
     if (!m_bot->IsWithinDistInMap(&obj, 50, true) && GetMaster()->isAlive() && !GetMaster()->IsTaxiFlying())
     {
+        m_bot->GetMotionMaster()->Clear();
         m_ignoreAIUpdatesUntilTime = time(0) + 6;
         PlayerbotChatHandler ch(GetMaster());
         if (!ch.teleport(*m_bot))
