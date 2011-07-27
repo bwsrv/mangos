b631644d89bb1fbb5c85463ceac82c3ab296b460
/dev/rsa <kris13@bk.ru>
2011-07-27 16:30:17 +0400

    [mr566] revert part of 11708 - wrong. close #114

--- src/game/Spell.cpp
+++ src/game/Spell.cpp
@@ -5098,10 +5098,6 @@ SpellCastResult Spell::CheckCast(bool strict)
                 return SPELL_FAILED_CASTER_AURASTATE;
         }
 
-        // totem immunity for channeled spells(needs to be before spell cast)
-        if (IsChanneledSpell(m_spellInfo) && target->GetTypeId() == TYPEID_UNIT && ((Creature*)target)->IsTotem())
-            return SPELL_FAILED_IMMUNE;
-
         bool non_caster_target = target != m_caster && !IsSpellWithCasterSourceTargetsOnly(m_spellInfo);
 
         if(non_caster_target)
