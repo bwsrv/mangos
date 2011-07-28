ac11adaa6ed24ad526720e938faaad5a23353943
Azerus <azerus21@mail.ru>
2011-07-28 20:30:33 +0600

    [bws402] Add negative effect for Sladran Snake Trap.

--- src/game/SpellEffects.cpp
+++ src/game/SpellEffects.cpp
@@ -8528,7 +8528,7 @@ void Spell::EffectScriptEffect(SpellEffectIndex eff_idx)
                 }
                 case 55299:                                 // Galdarah Transform to Troll!
                 {
-                    m_creature->RemoveAurasDueToSpell(55297);
+                    m_caster->RemoveAurasDueToSpell(55297);
                     return;
                 }
                 case 55693:                                 // Remove Collapsing Cave Aura
