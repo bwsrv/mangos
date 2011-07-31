638fd50351bd55312312fe92ad304e29f0036a58
mosst <Makcim_Chaos@ukr.net>
2011-07-31 11:29:09 +0300

    [mr711] Removed not necessary triggered spell for 32982.

--- sql_mr/mr00711_mangos_spell_hacks.sql
+++ sql_mr/mr00711_mangos_spell_hacks.sql
@@ -153,3 +153,15 @@ UPDATE `spell_dbc` SET
 `Effect2` = 0,
 `Comment` = 'Replacement for spell 27746 ("Азотистое ускорение")' 
 WHERE `Id` = 27746;
+
+-- Replace DBC values for spell 32982 - "Тотем элементаля огня" 
+-- 
+DELETE FROM `spell_dbc` WHERE `Id` = 32982;
+INSERT INTO `spell_dbc` 
+( `Id`, `Dispel`, `Mechanic`, `Attributes`, `AttributesEx`, `AttributesEx2`, `AttributesEx3`, `AttributesEx4`, `AttributesEx5`, `Stances`, `StancesNot`, `Targets`, `CastingTimeIndex`, `AuraInterruptFlags`, `ProcFlags`, `ProcChance`, `ProcCharges`, `MaxLevel`, `BaseLevel`, `SpellLevel`, `DurationIndex`, `RangeIndex`, `StackAmount`, `EquippedItemClass`, `EquippedItemSubClassMask`, `EquippedItemInventoryTypeMask`, `Effect1`, `Effect2`, `Effect3`, `EffectDieSides1`, `EffectDieSides2`, `EffectDieSides3`, `EffectRealPointsPerLevel1`, `EffectRealPointsPerLevel2`, `EffectRealPointsPerLevel3`, `EffectBasePoints1`, `EffectBasePoints2`, `EffectBasePoints3`, `EffectMechanic1`, `EffectMechanic2`, `EffectMechanic3`, `EffectImplicitTargetA1`, `EffectImplicitTargetA2`, `EffectImplicitTargetA3`, `EffectImplicitTargetB1`, `EffectImplicitTargetB2`, `EffectImplicitTargetB3`, `EffectRadiusIndex1`, `EffectRadiusIndex2`, `EffectRadiusIndex3`, `EffectApplyAuraName1`, `EffectApplyAuraName2`, `EffectApplyAuraName3`, `EffectAmplitude1`, `EffectAmplitude2`, `EffectAmplitude3`, `EffectMultipleValue1`, `EffectMultipleValue2`, `EffectMultipleValue3`, `EffectMiscValue1`, `EffectMiscValue2`, `EffectMiscValue3`, `EffectMiscValueB1`, `EffectMiscValueB2`, `EffectMiscValueB3`, `EffectTriggerSpell1`, `EffectTriggerSpell2`, `EffectTriggerSpell3`, `EffectSpellClassMaskA1`, `EffectSpellClassMaskA2`, `EffectSpellClassMaskA3`, `EffectSpellClassMaskB1`, `EffectSpellClassMaskB2`, `EffectSpellClassMaskB3`, `EffectSpellClassMaskC1`, `EffectSpellClassMaskC2`, `EffectSpellClassMaskC3`, `Comment`, `MaxTargetLevel`, `SpellFamilyName`, `SpellFamilyFlags1`, `SpellFamilyFlags2`, `SpellFamilyFlags3`, `MaxAffectedTargets`, `DmgClass`, `PreventionType`, `DmgMultiplier1`, `DmgMultiplier2`, `DmgMultiplier3`, `AreaGroupId`, `SchoolMask` )
+VALUES (32982, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 101, 0, 0, 0, 0, 4, 1, 0, -1, 0, 0, 46, 64, 28, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 32, 0, 0, 0, 0, 0, 8, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 15438, 0, 0, 61, 0, 32186, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, "Тотем элементаля огня", 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 1 );
+
+UPDATE `spell_dbc` SET 
+EffectTriggerSpell2 = 0,
+`Comment` = 'Replacement for spell 32982 ("Тотем элементаля огня")' 
+WHERE `Id` = 32982;
