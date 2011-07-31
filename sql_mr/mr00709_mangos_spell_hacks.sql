09344e7ac862f09fad7fa11d897b7db5109d64b0
mosst <Makcim_Chaos@ukr.net>
2011-07-31 10:43:30 +0300

    [mr709] Removed not necessary triggered spell for 49028.

--- sql_mr/mr00709_mangos_spell_hacks.sql
+++ sql_mr/mr00709_mangos_spell_hacks.sql
@@ -129,3 +129,15 @@ UPDATE `spell_dbc` SET
 `Effect1` = 0,
 `Comment` = 'Replacement for spell 70842 ("Барьер маны")' 
 WHERE `Id` = 70842;
+
+-- Replace DBC values for spell 49028 - "Танцующее руническое оружие" 
+-- 
+DELETE FROM `spell_dbc` WHERE `Id` = 49028;
+INSERT INTO `spell_dbc` 
+( `Id`, `Dispel`, `Mechanic`, `Attributes`, `AttributesEx`, `AttributesEx2`, `AttributesEx3`, `AttributesEx4`, `AttributesEx5`, `Stances`, `StancesNot`, `Targets`, `CastingTimeIndex`, `AuraInterruptFlags`, `ProcFlags`, `ProcChance`, `ProcCharges`, `MaxLevel`, `BaseLevel`, `SpellLevel`, `DurationIndex`, `RangeIndex`, `StackAmount`, `EquippedItemClass`, `EquippedItemSubClassMask`, `EquippedItemInventoryTypeMask`, `Effect1`, `Effect2`, `Effect3`, `EffectDieSides1`, `EffectDieSides2`, `EffectDieSides3`, `EffectRealPointsPerLevel1`, `EffectRealPointsPerLevel2`, `EffectRealPointsPerLevel3`, `EffectBasePoints1`, `EffectBasePoints2`, `EffectBasePoints3`, `EffectMechanic1`, `EffectMechanic2`, `EffectMechanic3`, `EffectImplicitTargetA1`, `EffectImplicitTargetA2`, `EffectImplicitTargetA3`, `EffectImplicitTargetB1`, `EffectImplicitTargetB2`, `EffectImplicitTargetB3`, `EffectRadiusIndex1`, `EffectRadiusIndex2`, `EffectRadiusIndex3`, `EffectApplyAuraName1`, `EffectApplyAuraName2`, `EffectApplyAuraName3`, `EffectAmplitude1`, `EffectAmplitude2`, `EffectAmplitude3`, `EffectMultipleValue1`, `EffectMultipleValue2`, `EffectMultipleValue3`, `EffectMiscValue1`, `EffectMiscValue2`, `EffectMiscValue3`, `EffectMiscValueB1`, `EffectMiscValueB2`, `EffectMiscValueB3`, `EffectTriggerSpell1`, `EffectTriggerSpell2`, `EffectTriggerSpell3`, `EffectSpellClassMaskA1`, `EffectSpellClassMaskA2`, `EffectSpellClassMaskA3`, `EffectSpellClassMaskB1`, `EffectSpellClassMaskB2`, `EffectSpellClassMaskB3`, `EffectSpellClassMaskC1`, `EffectSpellClassMaskC2`, `EffectSpellClassMaskC3`, `Comment`, `MaxTargetLevel`, `SpellFamilyName`, `SpellFamilyFlags1`, `SpellFamilyFlags2`, `SpellFamilyFlags3`, `MaxAffectedTargets`, `DmgClass`, `PreventionType`, `DmgMultiplier1`, `DmgMultiplier2`, `DmgMultiplier3`, `AreaGroupId`, `SchoolMask` )
+VALUES (49028, 0, 0, 0, 0, 0, 67109890, 0, 0, 0, 0, 0, 1, 0, 86288, 100, 0, 80, 55, 55, 29, 4, 0, 2, 173555, 0, 28, 6, 6, 1, 1, 1, 0, 0, 0, 0, 50706, 9, 0, 0, 0, 64, 1, 6, 0, 0, 0, 7, 0, 0, 0, 4, 4, 0, 0, 0, 1, 0, 0, 27893, 0, 0, 208, 0, 0, 0, 1206, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, "Танцующее руническое оружие", 0, 15, 1073741824, 0, 0, 0, 1, 1, 1, 1, 1, 0, 1 );
+
+UPDATE `spell_dbc` SET 
+EffectTriggerSpell2 = 0, 
+`Comment` = 'Replacement for spell 49028 ("Танцующее руническое оружие")' 
+WHERE `Id` = 49028;
