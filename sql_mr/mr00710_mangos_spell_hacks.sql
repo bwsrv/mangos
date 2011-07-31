56ce47dfbf5618e3b8734d7bba71cf80175fff38
mosst <Makcim_Chaos@ukr.net>
2011-07-31 11:02:04 +0300

    [mr710] Removed not necessary Effect 1: Id 6 (SPELL_EFFECT_APPLY_AURA) for spell 27746.

--- sql_mr/mr00710_mangos_spell_hacks.sql
+++ sql_mr/mr00710_mangos_spell_hacks.sql
@@ -141,3 +141,15 @@ UPDATE `spell_dbc` SET
 EffectTriggerSpell2 = 0, 
 `Comment` = 'Replacement for spell 49028 ("Танцующее руническое оружие")' 
 WHERE `Id` = 49028;
+
+-- Replace DBC values for spell 27746 - "Азотистое ускорение" 
+-- 
+DELETE FROM `spell_dbc` WHERE `Id` = 27746;
+INSERT INTO `spell_dbc` 
+( `Id`, `Dispel`, `Mechanic`, `Attributes`, `AttributesEx`, `AttributesEx2`, `AttributesEx3`, `AttributesEx4`, `AttributesEx5`, `Stances`, `StancesNot`, `Targets`, `CastingTimeIndex`, `AuraInterruptFlags`, `ProcFlags`, `ProcChance`, `ProcCharges`, `MaxLevel`, `BaseLevel`, `SpellLevel`, `DurationIndex`, `RangeIndex`, `StackAmount`, `EquippedItemClass`, `EquippedItemSubClassMask`, `EquippedItemInventoryTypeMask`, `Effect1`, `Effect2`, `Effect3`, `EffectDieSides1`, `EffectDieSides2`, `EffectDieSides3`, `EffectRealPointsPerLevel1`, `EffectRealPointsPerLevel2`, `EffectRealPointsPerLevel3`, `EffectBasePoints1`, `EffectBasePoints2`, `EffectBasePoints3`, `EffectMechanic1`, `EffectMechanic2`, `EffectMechanic3`, `EffectImplicitTargetA1`, `EffectImplicitTargetA2`, `EffectImplicitTargetA3`, `EffectImplicitTargetB1`, `EffectImplicitTargetB2`, `EffectImplicitTargetB3`, `EffectRadiusIndex1`, `EffectRadiusIndex2`, `EffectRadiusIndex3`, `EffectApplyAuraName1`, `EffectApplyAuraName2`, `EffectApplyAuraName3`, `EffectAmplitude1`, `EffectAmplitude2`, `EffectAmplitude3`, `EffectMultipleValue1`, `EffectMultipleValue2`, `EffectMultipleValue3`, `EffectMiscValue1`, `EffectMiscValue2`, `EffectMiscValue3`, `EffectMiscValueB1`, `EffectMiscValueB2`, `EffectMiscValueB3`, `EffectTriggerSpell1`, `EffectTriggerSpell2`, `EffectTriggerSpell3`, `EffectSpellClassMaskA1`, `EffectSpellClassMaskA2`, `EffectSpellClassMaskA3`, `EffectSpellClassMaskB1`, `EffectSpellClassMaskB2`, `EffectSpellClassMaskB3`, `EffectSpellClassMaskC1`, `EffectSpellClassMaskC2`, `EffectSpellClassMaskC3`, `Comment`, `MaxTargetLevel`, `SpellFamilyName`, `SpellFamilyFlags1`, `SpellFamilyFlags2`, `SpellFamilyFlags3`, `MaxAffectedTargets`, `DmgClass`, `PreventionType`, `DmgMultiplier1`, `DmgMultiplier2`, `DmgMultiplier3`, `AreaGroupId`, `SchoolMask` )
+VALUES (27746, 0, 0, 0, 131072, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 101, 0, 0, 0, 0, 21, 1, 0, -1, 0, 0, 6, 6, 0, 1, 0, 0, 0, 0, 0, 49, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 31, 23, 0, 0, 1000, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, "Азотистое ускорение", 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 1 );
+
+UPDATE `spell_dbc` SET 
+`Effect2` = 0,
+`Comment` = 'Replacement for spell 27746 ("Азотистое ускорение")' 
+WHERE `Id` = 27746;
--- src/shared/revision_R2.h
+++ src/shared/revision_R2.h
@@ -1,4 +1,4 @@
 #ifndef __REVISION_R2_H__
 #define __REVISION_R2_H__
- #define REVISION_R2 "709"
+ #define REVISION_R2 "710"
 #endif // __REVISION_R2_H__
