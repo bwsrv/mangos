08439017ee667b0a6563459c4db09b58ea26661d
mosst <Makcim_Chaos@ukr.net>
2011-07-31 11:41:42 +0300

    [mr712] Removed not necessary triggered spell for 69029.

--- sql_mr/mr00712_mangos_spell_hacks.sql
+++ sql_mr/mr00712_mangos_spell_hacks.sql
@@ -165,3 +165,15 @@ UPDATE `spell_dbc` SET
 EffectTriggerSpell2 = 0,
 `Comment` = 'Replacement for spell 32982 ("Тотем элементаля огня")' 
 WHERE `Id` = 32982;
+
+-- Replace DBC values for spell 69029 - "Замешательство преследователя" 
+-- 
+DELETE FROM `spell_dbc` WHERE `Id` = 69029;
+INSERT INTO `spell_dbc` 
+( `Id`, `Dispel`, `Mechanic`, `Attributes`, `AttributesEx`, `AttributesEx2`, `AttributesEx3`, `AttributesEx4`, `AttributesEx5`, `Stances`, `StancesNot`, `Targets`, `CastingTimeIndex`, `AuraInterruptFlags`, `ProcFlags`, `ProcChance`, `ProcCharges`, `MaxLevel`, `BaseLevel`, `SpellLevel`, `DurationIndex`, `RangeIndex`, `StackAmount`, `EquippedItemClass`, `EquippedItemSubClassMask`, `EquippedItemInventoryTypeMask`, `Effect1`, `Effect2`, `Effect3`, `EffectDieSides1`, `EffectDieSides2`, `EffectDieSides3`, `EffectRealPointsPerLevel1`, `EffectRealPointsPerLevel2`, `EffectRealPointsPerLevel3`, `EffectBasePoints1`, `EffectBasePoints2`, `EffectBasePoints3`, `EffectMechanic1`, `EffectMechanic2`, `EffectMechanic3`, `EffectImplicitTargetA1`, `EffectImplicitTargetA2`, `EffectImplicitTargetA3`, `EffectImplicitTargetB1`, `EffectImplicitTargetB2`, `EffectImplicitTargetB3`, `EffectRadiusIndex1`, `EffectRadiusIndex2`, `EffectRadiusIndex3`, `EffectApplyAuraName1`, `EffectApplyAuraName2`, `EffectApplyAuraName3`, `EffectAmplitude1`, `EffectAmplitude2`, `EffectAmplitude3`, `EffectMultipleValue1`, `EffectMultipleValue2`, `EffectMultipleValue3`, `EffectMiscValue1`, `EffectMiscValue2`, `EffectMiscValue3`, `EffectMiscValueB1`, `EffectMiscValueB2`, `EffectMiscValueB3`, `EffectTriggerSpell1`, `EffectTriggerSpell2`, `EffectTriggerSpell3`, `EffectSpellClassMaskA1`, `EffectSpellClassMaskA2`, `EffectSpellClassMaskA3`, `EffectSpellClassMaskB1`, `EffectSpellClassMaskB2`, `EffectSpellClassMaskB3`, `EffectSpellClassMaskC1`, `EffectSpellClassMaskC2`, `EffectSpellClassMaskC3`, `Comment`, `MaxTargetLevel`, `SpellFamilyName`, `SpellFamilyFlags1`, `SpellFamilyFlags2`, `SpellFamilyFlags3`, `MaxAffectedTargets`, `DmgClass`, `PreventionType`, `DmgMultiplier1`, `DmgMultiplier2`, `DmgMultiplier3`, `AreaGroupId`, `SchoolMask` )
+VALUES (69029, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 101, 0, 0, 0, 0, 29, 1, 0, -1, 0, 0, 6, 6, 6, 1, 1, 0, 0, 0, 0, -51, 49, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 33, 79, 284, 0, 0, 0, 0, 0, 0, 0, 127, 11, 0, 0, 0, 0, 0, 69357, 0, 0, 0, 0, 0, 0, 0, 0, 0, "Замешательство преследователя", 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 1 );
+
+UPDATE `spell_dbc` SET 
+EffectTriggerSpell3 = 0, 
+`Comment` = 'Replacement for spell 69029 ("Замешательство преследователя")' 
+WHERE `Id` = 69029;
--- src/shared/revision_R2.h
+++ src/shared/revision_R2.h
@@ -1,4 +1,4 @@
 #ifndef __REVISION_R2_H__
 #define __REVISION_R2_H__
- #define REVISION_R2 "711"
+ #define REVISION_R2 "712"
 #endif // __REVISION_R2_H__
