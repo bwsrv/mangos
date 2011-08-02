fa7d63752833861dc3601a55a08441adfd66ec6a
mosst <Makcim_Chaos@ukr.net>
2011-08-02 15:27:02 +0300

    Revert "[bws372] Restore build."
    This reverts commit b75c2ff28561154785b3043368b054ecee8d9658.

--- src/game/Transports.cpp
+++ src/game/Transports.cpp
@@ -179,7 +179,7 @@ void MapManager::LoadTransportCreatures()
             }
 
             if (transportUse == 1)
-                trans->AddNPCPassenger(data->id, data->posX, data->posY, data->posZ, data->orientation, TEAM_NONE);
+                trans->AddNPCPassenger(data->id, data->posX, data->posY, data->posZ, data->orientation, TEAM_NONE, data);
 
             break;
         }
@@ -669,7 +669,7 @@ void Transport::Update(uint32 update_diff, uint32 /*p_time*/)
     }
 }
 
-Creature* Transport::AddNPCPassenger(uint32 entry, float x, float y, float z, float o, Team team /*= TEAM_NONE*/)
+Creature* Transport::AddNPCPassenger(uint32 entry, float x, float y, float z, float o, Team team /*= TEAM_NONE*/, const CreatureData *data /*= NULL*/)
 {
     CreatureInfo const *cinfo = ObjectMgr::GetCreatureTemplate(entry);
     if (!cinfo)
@@ -680,7 +680,7 @@ Creature* Transport::AddNPCPassenger(uint32 entry, float x, float y, float z, fl
 
     CreatureCreatePos pos(map, x, y, z, o, GetPhaseMask());
 
-    if (!pCreature->Create(map->GenerateLocalLowGuid(HIGHGUID_UNIT), pos, cinfo, team))
+    if (!pCreature->Create(map->GenerateLocalLowGuid(HIGHGUID_UNIT), pos, cinfo, team, data))
     {
         delete pCreature;
         return NULL;
--- src/game/Transports.h
+++ src/game/Transports.h
@@ -41,7 +41,7 @@ class MANGOS_DLL_SPEC Transport : public GameObject
         typedef std::set<Unit*> UnitSet;
         UnitSet const& GetUnitPassengers() const { return _passengers; }
 
-        Creature* AddNPCPassenger(uint32 entry, float x, float y, float z, float o, Team team = TEAM_NONE);
+        Creature* AddNPCPassenger(uint32 entry, float x, float y, float z, float o, Team team = TEAM_NONE, const CreatureData *data = NULL);
 
         void BuildCreateUpdateBlockForPlayer(UpdateData* data, Player* target);
         void BuildMovementPacket(Map const* targetMap, bool isMoving = false);
