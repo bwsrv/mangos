980f899e6c84d669683b26ad1c0892efc84f4ea0
Shin Darth <shin.darth@hotmail.it>
2011-08-10 21:47:02 +0400

    [mr810] fix one possible crush in BG

--- src/game/BattleGround.cpp
+++ src/game/BattleGround.cpp
@@ -1206,6 +1206,8 @@ void BattleGround::RemovePlayerAtLeave(ObjectGuid guid, bool Transport, bool Sen
             plr->SpawnCorpseBones();
         }
     }
+    else
+        sObjectAccessor.ConvertCorpseForPlayer(guid);
 
     RemovePlayer(plr, guid);                                // BG subclass specific code
 
