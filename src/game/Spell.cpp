cc1c720b3ba3735cf025521ff25076ff8628c2a5
/dev/rsa <kris13@bk.ru>
2011-08-16 22:59:10 +0400

    [mr833] additional checks in DoAllEffect's

--- src/game/Spell.cpp
+++ src/game/Spell.cpp
@@ -981,8 +981,9 @@ void Spell::AddItemTarget(Item* pitem, SpellEffectIndex effIndex)
 
 void Spell::DoAllEffectOnTarget(TargetInfo *target)
 {
-    if (target->processed)                                  // Check target
+    if (!target || target->processed)                       // Check target
         return;
+
     target->processed = true;                               // Target checked in apply effects procedure
 
     // Get mask of effects for target
@@ -1410,8 +1411,10 @@ void Spell::DoSpellHitOnUnit(Unit *unit, uint32 effectMask)
 
 void Spell::DoAllEffectOnTarget(GOTargetInfo *target)
 {
-    if (target->processed)                                  // Check target
+
+    if (!target || target->processed)                       // Check target
         return;
+
     target->processed = true;                               // Target checked in apply effects procedure
 
     uint32 effectMask = target->effectMask;
