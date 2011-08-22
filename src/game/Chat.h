8e5adbd6acdbef1950c5562313a394b998f6dbb4
mosst <Makcim_Chaos@ukr.net>
2011-08-22 09:46:22 +0300

    Revert "[bws3] Full Implemented RFC System."
    This reverts commit c7007d41839826aed5e553d66ac2c4652740b67a.

--- src/game/Chat.h
+++ src/game/Chat.h
@@ -190,8 +190,6 @@ class MANGOS_DLL_SPEC ChatHandler
         bool HandleCastTargetCommand(char* args);
 
         bool HandleCharacterAchievementsCommand(char* args);
-        bool HandleCharacterChangeFactionCommand(char* args);
-        bool HandleCharacterChangeRaceCommand(char* args);
         bool HandleCharacterCustomizeCommand(char* args);
         bool HandleCharacterDeletedDeleteCommand(char* args);
         bool HandleCharacterDeletedListCommand(char* args);
