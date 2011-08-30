ff550675fb9bc8f185177d76787c32b9f808c88d
/dev/rsa <kris13@bk.ru>
2011-08-30 23:19:21 +0400

    [mr922] partially return to original mangos reset method, if difficulty changed. TESTING!

--- src/game/Map.cpp
+++ src/game/Map.cpp
@@ -1439,11 +1439,20 @@ bool DungeonMap::Reset(InstanceResetMethod method)
 
     if(HavePlayers())
     {
-        if(method == INSTANCE_RESET_ALL || method == INSTANCE_RESET_CHANGE_DIFFICULTY)
+        if (method == INSTANCE_RESET_ALL)
         {
             // notify the players to leave the instance so it can be reset
             for(MapRefManager::iterator itr = m_mapRefManager.begin(); itr != m_mapRefManager.end(); ++itr)
                 itr->getSource()->SendResetFailedNotify(GetId());
+        }
+        else if (method == INSTANCE_RESET_CHANGE_DIFFICULTY)
+        {
+            // notify the players to leave the instance so it can be reset
+            for(MapRefManager::iterator itr = m_mapRefManager.begin(); itr != m_mapRefManager.end(); ++itr)
+                itr->getSource()->SendResetFailedNotify(GetId());
+
+            m_unloadWhenEmpty = true;
+            m_resetAfterUnload = true;
         }
         else
         {
--- src/shared/revision_R2.h
+++ src/shared/revision_R2.h
@@ -1,4 +1,4 @@
 #ifndef __REVISION_R2_H__
 #define __REVISION_R2_H__
- #define REVISION_R2 "921"
+ #define REVISION_R2 "922"
 #endif // __REVISION_R2_H__
