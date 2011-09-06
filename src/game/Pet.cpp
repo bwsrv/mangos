0bf307d8fb92223de53bcc0e7989f8f4fa2a4df7
rsa <kris13@bk.ru>
2011-09-06 17:26:32 +0400

    [mr973] restore guardian/protector pet spell autouse possibility

--- src/game/Pet.cpp
+++ src/game/Pet.cpp
@@ -1766,6 +1766,20 @@ void Pet::InitPetCreateSpells()
 
     LearnPetPassives();
 
+    if (!isControlled())
+    {
+        for(uint32 x = 0; x <= GetSpellMaxIndex(); ++x)
+        {
+            if (uint32 spellId = GetSpell(x))
+            {
+                if (IsPassiveSpell(spellId))
+                    CastSpell(this, spellId, true);
+                else
+                    addSpell(spellId, ACT_ENABLED);
+            }
+        }
+    }
+
     CastPetAuras(false);
 }
 
@@ -1954,7 +1968,7 @@ uint8 Pet::GetMaxTalentPointsForLevel(uint32 level)
 
 void Pet::ToggleAutocast(uint32 spellid, bool apply)
 {
-    if(IsPassiveSpell(spellid) || !isControlled())
+    if (IsPassiveSpell(spellid))
         return;
 
     PetSpellMap::iterator itr = m_spells.find(spellid);
@@ -2906,6 +2920,7 @@ bool Pet::Summon()
         case GUARDIAN_PET:
         case PROTECTOR_PET:
         {
+            GetCharmInfo()->InitCharmCreateSpells();
             LoadCreatureAddon(true);
             RemoveByteFlag(UNIT_FIELD_BYTES_2, 1, UNIT_BYTE2_FLAG_FFA_PVP | UNIT_BYTE2_FLAG_SANCTUARY | UNIT_BYTE2_FLAG_PVP);
             SetUInt32Value(UNIT_NPC_FLAGS, GetCreatureInfo()->npcflag);
