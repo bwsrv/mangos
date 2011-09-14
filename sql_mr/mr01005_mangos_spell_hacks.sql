b213bec7c4bf71965b2139469d696ba48f26beda
Asprorpsa <romanoff.d@mail.ru>
2011-09-14 20:47:49 +0400

    [mr1005] fix 38307 && 59043. close #369

--- sql_mr/mr01005_mangos_spell_hacks.sql
+++ sql_mr/mr01005_mangos_spell_hacks.sql
@@ -240,3 +240,20 @@ UPDATE spell_dbc SET
 EffectApplyAuraName1 = 23
 -- Comment = 'Replacement for spell 42760 ("Гоблинское хлебово")'
 WHERE Id = 42760;
+
+-- 38307 && 59043 - positive spell's
+-- 
+DELETE FROM `spell_dbc` WHERE `Id` IN (59043, 38307);
+INSERT INTO `spell_dbc` (`Id`, `Category`, `Dispel`, `Mechanic`, `Attributes`, `AttributesEx`, `AttributesEx2`, `AttributesEx3`, `AttributesEx4`, `AttributesEx5`, `AttributesEx6`, `AttributesEx7`, `Stances`, `StancesNot`, `Targets`, `CasterAuraSpell`, `TargetAuraSpell`, `ExcludeCasterAuraSpell`, `ExcludeTargetAuraSpell`, `CastingTimeIndex`, `RecoveryTime`, `CategoryRecoveryTime`, `InterruptFlags`, `AuraInterruptFlags`, `ProcFlags`, `ProcChance`, `ProcCharges`, `MaxLevel`, `BaseLevel`, `SpellLevel`, `DurationIndex`, `RangeIndex`, `StackAmount`, `EquippedItemClass`, `EquippedItemSubClassMask`, `EquippedItemInventoryTypeMask`, `Effect1`, `Effect2`, `Effect3`, `EffectDieSides1`, `EffectDieSides2`, `EffectDieSides3`, `EffectRealPointsPerLevel1`, `EffectRealPointsPerLevel2`, `EffectRealPointsPerLevel3`, `EffectBasePoints1`, `EffectBasePoints2`, `EffectBasePoints3`, `EffectMechanic1`, `EffectMechanic2`, `EffectMechanic3`, `EffectImplicitTargetA1`, `EffectImplicitTargetA2`, `EffectImplicitTargetA3`, `EffectImplicitTargetB1`, `EffectImplicitTargetB2`, `EffectImplicitTargetB3`, `EffectRadiusIndex1`, `EffectRadiusIndex2`, `EffectRadiusIndex3`, `EffectApplyAuraName1`, `EffectApplyAuraName2`, `EffectApplyAuraName3`, `EffectAmplitude1`, `EffectAmplitude2`, `EffectAmplitude3`, `EffectMultipleValue1`, `EffectMultipleValue2`, `EffectMultipleValue3`, `EffectMiscValue1`, `EffectMiscValue2`, `EffectMiscValue3`, `EffectMiscValueB1`, `EffectMiscValueB2`, `EffectMiscValueB3`, `EffectTriggerSpell1`, `EffectTriggerSpell2`, `EffectTriggerSpell3`, `EffectSpellClassMaskA1`, `EffectSpellClassMaskA2`, `EffectSpellClassMaskA3`, `EffectSpellClassMaskB1`, `EffectSpellClassMaskB2`, `EffectSpellClassMaskB3`, `EffectSpellClassMaskC1`, `EffectSpellClassMaskC2`, `EffectSpellClassMaskC3`, `SpellIconID`, `Comment`, `MaxTargetLevel`, `SpellFamilyName`, `SpellFamilyFlags1`, `SpellFamilyFlags2`, `SpellFamilyFlags3`, `MaxAffectedTargets`, `DmgClass`, `PreventionType`, `DmgMultiplier1`, `DmgMultiplier2`, `DmgMultiplier3`, `AreaGroupId`, `SchoolMask`) VALUES
+(38307, 0, 2, 0, 0, 136, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 15, 0, 0, 101, 0, 0, 60, 60, 1, 4, 3, -1, 0, 0, 6, 0, 0, 1, 0, 0, 0, 0, 0, 61, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 189, 0, 0, 0, 0, 0, 0, 0, 0, 16777216, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 164, "", 0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 1, 0, 32),
+(59043, 0, 2, 0, 0, 136, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 15, 0, 0, 101, 0, 0, 60, 60, 1, 4, 3, -1, 0, 0, 6, 0, 0, 1, 0, 0, 0, 0, 0, 154, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 189, 0, 0, 0, 0, 0, 0, 0, 0, 16777216, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 164, "", 0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 1, 0, 32);
+
+UPDATE `spell_dbc` SET 
+`AttributesEx` = `AttributesEx` &~ 128,
+`Comment` = 'Replacement for spell 38307 ("Ночная тьма")' 
+WHERE `Id` = 38307;
+
+UPDATE `spell_dbc` SET 
+`AttributesEx` = `AttributesEx` &~ 128, 
+`Comment` = 'Replacement for spell 59043 ("Непроглядная ночь")' 
+WHERE `Id` = 59043;
--- src/shared/revision_R2.h
+++ src/shared/revision_R2.h
@@ -1,4 +1,4 @@
 #ifndef __REVISION_R2_H__
 #define __REVISION_R2_H__
- #define REVISION_R2 "1004"
+ #define REVISION_R2 "1005"
 #endif // __REVISION_R2_H__
