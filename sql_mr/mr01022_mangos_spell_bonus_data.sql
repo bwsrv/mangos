4d3cecdf83efaa787627ec27cb5bda446b7d856d
tibbi <tibbi@centrum.sk>
2011-09-17 16:19:03 +0400

    [mr1022] fix vampiric touch dispell damage

--- sql_mr/mr01022_mangos_spell_bonus_data.sql
+++ sql_mr/mr01022_mangos_spell_bonus_data.sql
@@ -16,7 +16,11 @@ VALUES
     (54158, 0, 0, 0, 0,'Judgement'),
     -- Glyph of  Prayer of Healing
     (56161, 0, 0, 0, 0,'Glyph of Prayer of Healing'),
-	-- Glyph of Chains of Ice (it's correct, but bonus value is simple)
-	(58621, 0, 0, 0.08, 0, 'Glyph of Chains of Ice'),
-	-- Priest - Divine Hymn
-	(64844, 0.564, 0, 0, 0, 'Priest - Divine Hymn');
+    -- Glyph of Chains of Ice (it's correct, but bonus value is simple)
+    (58621, 0, 0, 0.08, 0, 'Glyph of Chains of Ice'),
+    -- Priest - Divine Hymn
+    (64844, 0.564, 0, 0, 0, 'Priest - Divine Hymn');
+
+-- Priest - Vampiric Touch Dispel
+DELETE FROM `spell_bonus_data` WHERE `entry` = 64085;
+INSERT INTO `spell_bonus_data` VALUES ('64085', '2.4', '0', '0', '0', 'Priest - Vampiric Touch Dispel');
--- src/shared/revision_R2.h
+++ src/shared/revision_R2.h
@@ -1,4 +1,4 @@
 #ifndef __REVISION_R2_H__
 #define __REVISION_R2_H__
- #define REVISION_R2 "1021"
+ #define REVISION_R2 "1022"
 #endif // __REVISION_R2_H__
