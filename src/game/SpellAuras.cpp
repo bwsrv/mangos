d48fc3159bf9abfe9bdde43116c97d9aedae60a9
rsa <kris13@bk.ru>
2011-09-22 15:07:36 +0400

    [mr1043] fix possible crush in CleanupTriggeredSpells

--- src/game/SpellAuras.cpp
+++ src/game/SpellAuras.cpp
@@ -10225,6 +10225,9 @@ void SpellAuraHolder::_RemoveSpellAuraHolder()
 
 void SpellAuraHolder::CleanupTriggeredSpells()
 {
+    if (!m_spellProto)
+        return;
+
     for (int32 i = 0; i < MAX_EFFECT_INDEX; ++i)
     {
         if (!m_spellProto->EffectApplyAuraName[i])
