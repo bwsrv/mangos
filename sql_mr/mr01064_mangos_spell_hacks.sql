02336c865892abc8353bb06b5e3f679b0ac41911
Feanordev <feanordev@gmail.com>
2011-09-23 20:51:10 +0400

    [mr1064] correct to 1050

--- sql_mr/mr01064_mangos_spell_hacks.sql
+++ sql_mr/mr01064_mangos_spell_hacks.sql
@@ -282,3 +282,15 @@ VALUES (47939, 0, 0, 0, 0, 0, 4, 0, 0, 0, 0, 0, 0, 0, 0, 1519, 0, 0, 0, 0, 5, 15
 UPDATE `spell_dbc` SET
 `EffectImplicitTargetA2`= 1
 WHERE `Id`= 47939;
+
+-- Replace DBC values for spell 55689 - "Glyph of Shadow" 
+-- 
+DELETE FROM `spell_dbc` WHERE `Id` = 55689;
+INSERT INTO `spell_dbc` 
+( `Id`, `Category`, `Dispel`, `Mechanic`, `Attributes`, `AttributesEx`, `AttributesEx2`, `AttributesEx3`, `AttributesEx4`, `AttributesEx5`, `AttributesEx6`, `AttributesEx7`, `Stances`, `StancesNot`, `Targets`, `RequiresSpellFocus`, `CasterAuraSpell`, `TargetAuraSpell`, `ExcludeCasterAuraSpell`, `ExcludeTargetAuraSpell`, `CastingTimeIndex`, `RecoveryTime`, `CategoryRecoveryTime`, `InterruptFlags`, `AuraInterruptFlags`, `ProcFlags`, `ProcChance`, `ProcCharges`, `MaxLevel`, `BaseLevel`, `SpellLevel`, `DurationIndex`, `RangeIndex`, `StackAmount`, `EquippedItemClass`, `EquippedItemSubClassMask`, `EquippedItemInventoryTypeMask`, `Effect1`, `Effect2`, `Effect3`, `EffectDieSides1`, `EffectDieSides2`, `EffectDieSides3`, `EffectRealPointsPerLevel1`, `EffectRealPointsPerLevel2`, `EffectRealPointsPerLevel3`, `EffectBasePoints1`, `EffectBasePoints2`, `EffectBasePoints3`, `EffectMechanic1`, `EffectMechanic2`, `EffectMechanic3`, `EffectImplicitTargetA1`, `EffectImplicitTargetA2`, `EffectImplicitTargetA3`, `EffectImplicitTargetB1`, `EffectImplicitTargetB2`, `EffectImplicitTargetB3`, `EffectRadiusIndex1`, `EffectRadiusIndex2`, `EffectRadiusIndex3`, `EffectApplyAuraName1`, `EffectApplyAuraName2`, `EffectApplyAuraName3`, `EffectAmplitude1`, `EffectAmplitude2`, `EffectAmplitude3`, `EffectMultipleValue1`, `EffectMultipleValue2`, `EffectMultipleValue3`, `EffectItemType1`, `EffectItemType2`, `EffectMiscValue1`, `EffectMiscValue2`, `EffectMiscValue3`, `EffectMiscValueB1`, `EffectMiscValueB2`, `EffectMiscValueB3`, `EffectTriggerSpell1`, `EffectTriggerSpell2`, `EffectTriggerSpell3`, `EffectSpellClassMaskA1`, `EffectSpellClassMaskA2`, `EffectSpellClassMaskA3`, `EffectSpellClassMaskB1`, `EffectSpellClassMaskB2`, `EffectSpellClassMaskB3`, `EffectSpellClassMaskC1`, `EffectSpellClassMaskC2`, `EffectSpellClassMaskC3`, `SpellIconID`, `Comment`, `MaxTargetLevel`, `SpellFamilyName`, `SpellFamilyFlags1`, `SpellFamilyFlags2`, `SpellFamilyFlags3`, `MaxAffectedTargets`, `DmgClass`, `PreventionType`, `DmgMultiplier1`, `DmgMultiplier2`, `DmgMultiplier3`, `AreaGroupId`, `SchoolMask` )
+VALUES (55689, 0, 0, 0, 192, 0, 0, 67108864, 0, 0, 0, 0, 134217728, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 65536, 100, 0, 0, 0, 0, 0, 1, 0, -1, 0, 0, 6, 0, 0, 1, 0, 0, 0, 0, 0, 99, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 42, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 61792, 0, 0, 0, 64, 0, 0, 64, 0, 0, 0, 0, 1, "Glyph of Shadow", 0, 6, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 1 );
+
+UPDATE `spell_dbc` SET
+`Stances` = 0,
+`Comment` = 'Replacement for spell 55689 ("Glyph of Shadow") ' 
+WHERE `Id` = 55689;
--- src/shared/revision_R2.h
+++ src/shared/revision_R2.h
@@ -1,4 +1,4 @@
 #ifndef __REVISION_R2_H__
 #define __REVISION_R2_H__
- #define REVISION_R2 "1063"
+ #define REVISION_R2 "1064"
 #endif // __REVISION_R2_H__
