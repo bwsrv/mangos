ded0537d65221897f312e247bd372786e2743f0f
PSZ <zavatski@gmail.com>
2011-10-05 17:23:59 +0300

    [mr1104] Fixed SQL query for GOSSIP_OPTION_MAILBOX support

--- sql_mr/mr01096_mangos_gossip_mailbox.sql
+++
@@ -1,7 +0,0 @@
--- mailbox option to pet's menu
-REPLACE INTO `gossip_menu_option` (`menu_id`, `id`, `option_icon`, `option_text`, `option_id`, `npc_option_npcflag`, `action_menu_id`, `action_poi_id`, `action_script_id`, `box_coded`, `box_money`, `box_text`, `cond_1`, `cond_1_val_1`, `cond_1_val_2`, `cond_2`, `cond_2_val_1`, `cond_2_val_2`, `cond_3`, `cond_3_val_1`, `cond_3_val_2`) VALUES
-(0, 0, 0, 'Visit a mailbox.', 19, 1, 0, 0, 0, 0, 0, '', 0, 0, 0, 0, 0, 0, 0, 0, 0);
-
-REPLACE INTO `gossip_menu_option` (`menu_id`, `id`, `option_icon`, `option_text`, `option_id`, `npc_option_npcflag`, `action_menu_id`, `action_poi_id`, `action_script_id`, `box_coded`, `box_money`, `box_text`, `cond_1`, `cond_1_val_1`, `cond_1_val_2`, `cond_2`, `cond_2_val_1`, `cond_2_val_2`, `cond_3`, `cond_3_val_1`, `cond_3_val_2`) VALUES
-(10238, 0, 0, 'Visit a mailbox.', 19, 1, 0, 0, 0, 0, 0, '', 0, 0, 0, 0, 0, 0, 0, 0, 0),
-(10239, 0, 0, 'Visit a mailbox.', 19, 1, 0, 0, 0, 0, 0, '', 0, 0, 0, 0, 0, 0, 0, 0, 0);
---
+++ sql_mr/mr01104_mangos_gossip_mailbox.sql
@@ -0,0 +1,4 @@
+-- mailbox option to pet's menu
+REPLACE INTO gossip_menu_option (menu_id, id, option_icon, option_text, option_id, npc_option_npcflag, action_menu_id, action_poi_id, action_script_id, box_coded, box_money, box_text, cond_1, cond_1_val_1, cond_1_val_2, cond_2, cond_2_val_1, cond_2_val_2, cond_3, cond_3_val_1, cond_3_val_2) VALUES
+(10318, 2, 0, 'Visit a mailbox.', 19, 1, -1, 0, 0, 0, 0, '', 11, 67334, 0, 20, 2817, 0, 0, 0, 0),
+(33239, 2, 0, 'Visit a mailbox.', 19, 1, -1, 0, 0, 0, 0, '', 11, 67401, 0, 20, 2816, 0, 0, 0, 0);
