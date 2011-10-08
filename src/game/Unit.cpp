0260c3e02b6ce76a70d9bb1db6b233a367502168
say1981 <say1981@ukr.net>
2011-10-08 13:01:00 +0300

    [mr1109] Fix an issue when player for some seconds continued uncontrolled movement when confused (polymorph, fear etc) aura was expired. Closes #410.

--- src/game/Unit.cpp
+++ src/game/Unit.cpp
@@ -11318,6 +11318,7 @@ void Unit::SetConfused(bool apply, ObjectGuid casterGuid, uint32 spellID)
     {
         RemoveFlag(UNIT_FIELD_FLAGS, UNIT_FLAG_CONFUSED);
 
+        StopMoving();
         GetMotionMaster()->MovementExpired(false);
 
         if (GetTypeId() != TYPEID_PLAYER && isAlive())
