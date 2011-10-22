53aad1f3ff5fa6aabd0307b1c02e60f8a182b47f
unknown <pc@pc-PC.(none)>
2011-10-21 21:29:45 -0700

    [mr1157] forgot rev..

--- sql_mr/mr01157_mangos_faction_change_reputations.sql
+++ sql_mr/mr01157_mangos_faction_change_reputations.sql
@@ -33,4 +33,3 @@ INSERT INTO `player_factionchange_reputations` VALUES ('0', '54', 'Gnomeregan Ex
 INSERT INTO `player_factionchange_reputations` VALUES ('0', '69', 'Darnassus', '0', '68', 'Undercity');
 INSERT INTO `player_factionchange_reputations` VALUES ('0', '72', 'Stormwind', '0', '76', 'Orgrimmar');
 INSERT INTO `player_factionchange_reputations` VALUES ('0', '930', 'Exodar', '0', '911', 'Silvermoon City');
-
