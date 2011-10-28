90b054874e4fcc00beae426d93b76cc7879a0d0c
Reamer <philipp.dallig@web.de>
2011-10-28 21:03:31 +0200

    [1182] New TemporarySummon Type.
    In my eyes good for manuell summon in scripts.
    Example: Boss summon add, this add despawn when corpse vanish or add is a specific time outfight(if bossfight fails)
    Best time in my eyes 2-5 minutes

--- src/game/Object.h
+++ src/game/Object.h
@@ -52,7 +52,8 @@ enum TempSummonType
     TEMPSUMMON_CORPSE_DESPAWN              = 5,             // despawns instantly after death
     TEMPSUMMON_CORPSE_TIMED_DESPAWN        = 6,             // despawns after a specified time after death
     TEMPSUMMON_DEAD_DESPAWN                = 7,             // despawns when the creature disappears
-    TEMPSUMMON_MANUAL_DESPAWN              = 8              // despawns when UnSummon() is called
+    TEMPSUMMON_MANUAL_DESPAWN              = 8,             // despawns when UnSummon() is called
+    TEMPSUMMON_TIMED_DESPAWN_OUT_OF_COMBAT_OR_DEAD_DESPAWN = 9 // despawns after a specified time after the creature is out of combat OR when the creature disappears
 };
 
 enum PhaseMasks
