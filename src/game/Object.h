07a69f66947ab9a04981ff8a8f25a231beddf482
/dev/rsa <kris13@bk.ru>
2011-11-05 18:04:32 +0400

    [mr1212] remove hardcoded value from cell::visit. make MAX_VISIBILITY_DISTANCE 500 instead of 333 (may require more CPU time!). ready to realization of "In frostmourne" event for Lich King encounter (ICC).

--- src/game/Object.h
+++ src/game/Object.h
@@ -26,17 +26,10 @@
 #include "ObjectGuid.h"
 #include "Camera.h"
 #include "ObjectLock.h"
+#include "SharedDefines.h"
 
 #include <set>
 #include <string>
-
-#define CONTACT_DISTANCE            0.5f
-#define INTERACTION_DISTANCE        5.0f
-#define ATTACK_DISTANCE             5.0f
-#define MAX_VISIBILITY_DISTANCE     333.0f      // max distance for visible object show, limited in 333 yards
-#define DEFAULT_VISIBILITY_DISTANCE 90.0f       // default visible distance, 90 yards on continents
-#define DEFAULT_VISIBILITY_INSTANCE 120.0f      // default visible distance in instances, 120 yards
-#define DEFAULT_VISIBILITY_BGARENAS 180.0f      // default visible distance in BG/Arenas, 180 yards
 
 #define DEFAULT_WORLD_OBJECT_SIZE   0.388999998569489f      // currently used (correctly?) for any non Unit world objects. This is actually the bounding_radius, like player/creature from creature_model_data
 #define DEFAULT_OBJECT_SCALE        1.0f                    // player/item scale as default, npc/go from database, pets from dbc
