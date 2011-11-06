fe7fa10edb94c96ff97b9d26da7fe30995a4ac52
tibbi <tibbi@centrum.sk>
2011-11-05 22:39:29 -0700

    [mr1214] correcting orientations after teleporting to bg/arenas

---
+++ sql_mr/mr01214_mangos_battleground_template.sql
@@ -0,0 +1,14 @@
+-- Alterac Valley
+UPDATE battleground_template SET AllianceStartO = 3.6, HordeStartO = 1.24 WHERE id = 1;
+-- Warsong Gulch
+UPDATE battleground_template SET HordeStartO = 0 WHERE id = 2;
+-- Nagrand Arena
+UPDATE battleground_template SET AllianceStartO = 5.21, HordeStartO = 2.06 WHERE id = 4;
+-- Blade's Edge Arena
+UPDATE battleground_template SET AllianceStartO = 4, HordeStartO = 0.903205 WHERE id = 5;
+-- Ruins of Lordaeron
+UPDATE battleground_template SET AllianceStartO = 4.83, HordeStartO = 1.7 WHERE id = 8;
+-- Dalaran Sewers
+UPDATE battleground_template SET HordeStartO = 3.14 WHERE id = 10;
+-- The Ring of Valor
+UPDATE battleground_template SET AllianceStartO = 4.72, HordeStartO = 1.6 WHERE id = 11;
