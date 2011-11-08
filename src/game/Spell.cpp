e6e798629e8a08fd25e40bb56f5d9ef7ff2d0784
/dev/rsa <kris13@bk.ru>
2011-11-08 19:53:19 +0400

    [mr1220] fix DK Gargoyle summon after implement TARGET_TOTEM_* target set methods. Thx to say1981. Close #492

--- src/game/Spell.cpp
+++ src/game/Spell.cpp
@@ -1954,6 +1954,9 @@ void Spell::SetTargetMap(SpellEffectIndex effIndex, uint32 targetMode, UnitList&
             {
                 // caster included here?
                 FillAreaTargets(targetUnitMap, radius, PUSH_DEST_CENTER, SPELL_TARGETS_ALL);
+
+                if (targetUnitMap.empty());
+                    targetUnitMap.push_back(m_caster);
             }
             else if (IsPositiveSpell(m_spellInfo->Id))
                     targetUnitMap.push_back(m_caster);
