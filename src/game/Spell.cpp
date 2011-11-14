15044d011968eedb3b0702e1487516f7df662084
mosst <Makcim_Chaos@ukr.net>
2011-11-14 20:16:10 +0200

    [a133] Hackfixed Slam not to cost 30 rage.
    Spell seems similiar to Shattering Throw, maybe there is some good solution for these spells and their very strange mechanics... :/
    
    Conflicts:
    
    	src/shared/revision_andeeria_nr.h

--- src/game/Spell.cpp
+++ src/game/Spell.cpp
@@ -3266,6 +3266,10 @@ void Spell::prepare(SpellCastTargets const* targets, Aura* triggeredByAura)
     // Fill cost data
     m_powerCost = CalculatePowerCost(m_spellInfo, m_caster, this, m_CastItem);
 
+    // Slam (hacky hacky hack, double rage cost fix)
+    if (m_spellInfo->Id == 50782)
+        m_powerCost = 0;
+
     SpellCastResult result = CheckCast(true);
     if (result != SPELL_CAST_OK && !IsAutoRepeat())          //always cast autorepeat dummy for triggering
     {
