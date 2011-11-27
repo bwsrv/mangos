a29b952cd5a6cb0c8be3f207db021f648c85ec2c
boxa <vb.i@mail.ru>
2011-11-28 01:44:59 +0400

    [mr1315] Finally fix damage to GO (like gates in SOTA). close #524

--- src/game/Spell.cpp
+++ src/game/Spell.cpp
@@ -6436,7 +6436,7 @@ SpellCastResult Spell::CheckCast(bool strict)
     }
 
     // check LOS for ground targeted spells
-    if (!m_targets.getUnitTarget() && !m_targets.getGOTarget() && !m_targets.getItemTarget())
+    if (!(m_spellInfo->AttributesEx2 & SPELL_ATTR_EX2_IGNORE_LOS) && !m_targets.getUnitTarget() && !m_targets.getGOTarget() && !m_targets.getItemTarget())
     {
         if (m_targets.m_destX && m_targets.m_destY && m_targets.m_destZ && !m_caster->IsWithinLOS(m_targets.m_destX, m_targets.m_destY, m_targets.m_destZ))
             return SPELL_FAILED_LINE_OF_SIGHT;
