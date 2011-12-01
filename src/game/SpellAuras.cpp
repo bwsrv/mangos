667f998fee90513f170b74e2aa27fff50c9fadcc
rsa <kris13@bk.ru>
2011-12-01 14:57:51 +0400

    [mr1357] add one exclude to areaaura. based on Undergarun's dumps.

--- src/game/SpellAuras.cpp
+++ src/game/SpellAuras.cpp
@@ -787,6 +787,9 @@ void Aura::AreaAuraUpdate(uint32 diff)
                 if (!i_target)
                     continue;
 
+                if (i_target->GetTypeId() == TYPEID_PLAYER && ((Player*)i_target)->IsBeingTeleportedFar())
+                    continue;
+
                 if (i_target->IsImmuneToSpell(GetSpellProto()))
                     continue;
                 else
@@ -799,7 +802,7 @@ void Aura::AreaAuraUpdate(uint32 diff)
                         if (!holder || holder->IsDeleted())
                             continue;
 
-                        Aura *aur = holder->GetAuraByEffectIndex(m_effIndex);
+                        Aura* aur = holder->GetAuraByEffectIndex(m_effIndex);
 
                         if (!aur)
                             continue;
