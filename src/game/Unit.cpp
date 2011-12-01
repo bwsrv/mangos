a0e571b8c64017f1c6aa6464b746b2333805c475
fallenangelX <iceis2@hotmail.com>
2011-12-01 09:06:46 -0500

    Revert "temp fix for mounts with accessories"
    This reverts commit 582aee4ac79d5eee94212756c529e075b6c6f650.

--- src/game/Unit.cpp
+++ src/game/Unit.cpp
@@ -8760,9 +8760,6 @@ void Unit::Mount(uint32 mount, uint32 spellId, uint32 vehicleId, uint32 creature
         {
             SetVehicleId(vehicleId);
             GetVehicleKit()->Reset();
-
-            // mounts can also have accessories
-            GetVehicleKit()->InstallAllAccessories(creatureEntry);
         }
     }
 }
