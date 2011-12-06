7165eaed1db6fd20072219fc0cd0c95040e7898d
PSZ <zavatski@gmail.com>
2011-12-06 14:12:08 +0200

    [mr1378] Fixed one crash in Playerbot

--- src/game/playerbot/PlayerbotAI.cpp
+++ src/game/playerbot/PlayerbotAI.cpp
@@ -2102,6 +2102,9 @@ void PlayerbotAI::GetDuelTarget(Unit* forcedTarget)
 
 void PlayerbotAI::DoNextCombatManeuver()
 {
+    if (!m_bot)
+        return;
+
     // check for new targets
     if (m_ScenarioType == SCENARIO_DUEL)
         GetDuelTarget(GetMaster());
