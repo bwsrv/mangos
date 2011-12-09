9e5395780cd8201d583defc2efa374909af8eb96
tibbi <tibbi@centrum.sk>
2011-12-09 21:58:30 +0100

    [mr1396] Dont remove water-walk, feather_fall, hover effect, if the player has more than 1 aura for it by feanordev

--- src/game/SpellAuras.cpp
+++ src/game/SpellAuras.cpp
@@ -3729,6 +3729,10 @@ void Aura::HandleAuraWaterWalk(bool apply, bool Real)
     if(!Real)
         return;
 
+    // Dont remove flag if player has another water-walk effect
+    if (!apply && GetTarget()->HasAuraType(SPELL_AURA_WATER_WALK))
+        return;
+
     WorldPacket data;
     if (apply)
         data.Initialize(SMSG_MOVE_WATER_WALK, 8+4);
@@ -3744,7 +3748,16 @@ void Aura::HandleAuraFeatherFall(bool apply, bool Real)
     // only at real add/remove aura
     if(!Real)
         return;
+
     Unit *target = GetTarget();
+
+    if (!apply)
+    {
+        // do not remove unit flag if there are more than this auraEffect of that kind on unit on unit
+        if (target->HasAuraType(SPELL_AURA_FEATHER_FALL))
+            return;
+    }
+
     WorldPacket data;
     if (apply)
         data.Initialize(SMSG_MOVE_FEATHER_FALL, 8+4);
@@ -3783,6 +3796,13 @@ void Aura::HandleAuraHover(bool apply, bool Real)
     // only at real add/remove aura
     if(!Real)
         return;
+
+    if (!apply)
+    {
+        // do not remove unit flag if there are more than this auraEffect of that kind on unit on unit
+        if (GetTarget()->HasAuraType(SPELL_AURA_HOVER))
+            return;
+    }
 
     WorldPacket data;
     if (apply)
