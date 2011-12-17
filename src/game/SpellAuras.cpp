6d5bd9803f9e26d91148536db975050f43f02fee
/dev/rsa <kris13@bk.ru>
2011-12-17 15:41:47 +0400

    [mr1435] fix 11866

--- src/game/SpellAuras.cpp
+++ src/game/SpellAuras.cpp
@@ -3023,15 +3023,9 @@ void Aura::HandleAuraDummy(bool apply, bool Real)
             }
             case 43681:                                     // Inactive
             {
-                if (!target || target->GetTypeId() != TYPEID_PLAYER || m_removeMode != AURA_REMOVE_BY_EXPIRE)
+                if (!target || target->GetTypeId() != TYPEID_PLAYER)
                     return;
 
-                if (target->GetMap()->IsBattleGround())
-                    ((Player*)target)->LeaveBattleground();
-                return;
-            }
-            case 43681:                                     // Inactive
-            {
                 if (m_removeMode == AURA_REMOVE_BY_EXPIRE && target->GetTypeId() == TYPEID_PLAYER)
                     ((Player*)target)->ToggleAFK();
                 return;
