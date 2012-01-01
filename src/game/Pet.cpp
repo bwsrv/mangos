dc57892122642eba27ee83d105ccd2cd0e5c050d
rsa <kris13@bk.ru>
2012-01-01 10:47:25 +0400

    Revert "[mr1505] Correcting mr1500 after tests"
    This reverts commit 61a293233bcc63558e6faebc0be8772a6071fdd5.

--- src/game/Pet.cpp
+++ src/game/Pet.cpp
@@ -2036,7 +2036,7 @@ bool Pet::IsPermanentPetFor(Player* owner)
                 case CLASS_DEATH_KNIGHT:
                     return GetCreatureInfo()->type == CREATURE_TYPE_UNDEAD;
                 case CLASS_MAGE:
-                    return GetCreatureInfo()->Entry == 510;
+                    return GetCreatureInfo()->Entry == 37994;
                 default:
                     return false;
             }
