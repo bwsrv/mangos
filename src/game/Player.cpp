cf52ae0699c467d2ce45fe167a768dc6b1941f3a
tibbi <tibbi@centrum.sk>
2012-01-18 18:18:36 +0100

    [mr1577] correcting talent reset, close #668

--- src/game/Player.cpp
+++ src/game/Player.cpp
@@ -3950,7 +3950,14 @@ bool Player::resetTalents(bool no_cost, bool all_specs)
 
         for (int j = 0; j < MAX_TALENT_RANK; ++j)
             if (talentInfo->RankID[j])
+            {
                 removeSpell(talentInfo->RankID[j],!IsPassiveSpell(talentInfo->RankID[j]),false);
+
+                SpellEntry const *spellInfo = sSpellStore.LookupEntry(talentInfo->RankID[j]);
+                for (int k = 0; k < MAX_EFFECT_INDEX; ++k)
+                    if (spellInfo->EffectTriggerSpell[k])
+                        removeSpell(spellInfo->EffectTriggerSpell[k]);
+            }
 
         iter = m_talents[m_activeSpec].begin();
     }
