d1e54392a6a77a6fc213302ed8534d904f0f6922
rsa <kris13@bk.ru>
2012-01-25 15:43:34 +0400

    [mr1635] not need collect deleted holders for non-player unit (after testing, thx to Undergarun).

--- src/game/Unit.cpp
+++ src/game/Unit.cpp
@@ -12668,9 +12668,10 @@ void Unit::CleanupDeletedHolders(bool force)
     {
         for (SpellAuraHolderSet::iterator iter = m_deletedHolders.begin(); iter != m_deletedHolders.end();)
         {
-            if ((*iter) && !(*iter)->IsInUse())
+            if ((*iter) && (!(*iter)->IsInUse() || GetTypeId() != TYPEID_PLAYER))
             {
-                m_deletedHolders.erase(*iter++);
+                m_deletedHolders.erase(*iter);
+                iter = m_deletedHolders.begin();
             }
             else
                 ++iter;
--- src/shared/revision_R2.h
+++ src/shared/revision_R2.h
@@ -1,4 +1,4 @@
 #ifndef __REVISION_R2_H__
 #define __REVISION_R2_H__
- #define REVISION_R2 "1634"
+ #define REVISION_R2 "1635"
 #endif // __REVISION_R2_H__
