0a4f22d32a43012518c3e84b3a1ea44314c2d9c0
mosst <Makcim_Chaos@Ukr.net>
2012-01-29 21:37:25 +0200

    Revert "[bws38] Implemented Name Announce by Taumer."
    This reverts commit 67ac93f4c45af5f2c4ea0063c6fd2da0411b58a3.
    
    Conflicts:
    
    	sql_bws/[bws00038]_mangos_name_announce.sql
    	src/game/Chat.cpp
    	src/game/Chat.h

--- sql_bws/[bws00038]_mangos_name_announce.sql
+++
@@ -1,6 +0,0 @@
-REPLACE INTO `command` VALUES
-   ('nameannounce',1,'Syntax: .nameannounce $MessageToBroadcast\r\n\r\nSend a global message to all players online in chat log with colored sender\'s name.');
-REPLACE INTO `mangos_string` VALUES
-   (10000,'|c1f40af20 <Mod>|cffff0000[%s]|c1f40af20 announce:|cffffff00 %s|r',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),
-   (10001,'|c1f40af20 <GM>|cffff0000[%s]|c1f40af20 announce:|cffffff00 %s|r',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL), 
-   (10002,'|c1f40af20 <Admin>|cffff0000[%s]|c1f40af20 announce:|cffffff00 %s|r',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL);
