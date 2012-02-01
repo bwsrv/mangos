bac9feec2aaad5fa0b3576ee27debc47b518240c
mosst <Makcim_Chaos@Ukr.net>
2012-02-02 00:34:00 +0200

    [mr1662] Disable enter some instances while encounter is in progress.

---
+++ sql_mr/mr01662_mangos_areatrigger_teleport.sql
@@ -0,0 +1,29 @@
+UPDATE areatrigger_teleport SET combat_mode = 1 WHERE id IN
+-- ICC IceCrown Citadel
+(5670, 
+-- Trial of the Crusader
+5503,
+-- Trial of the Champion
+5505,
+-- Vault of Archavon
+5258,
+-- Ulduar Raid
+5379,
+-- Naxxramas
+5191, 5192, 5193, 5194,
+-- Sunwell Plateau 
+4889,
+-- Zul'Aman
+4738,
+-- Black Temple
+4598,
+-- Gruul\'s Lair 
+4535, 
+-- The Eye
+4470,
+-- Serpentshrine Cavern
+4416,
+-- Karazhan
+4131, 4135,
+-- Zul'Gurub 
+3928);
