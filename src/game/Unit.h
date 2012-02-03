48b96127c064d9764d0d9c9827e63c40a781d5df
Inquisitor <ssd_@ukr.net>
2012-02-03 18:34:12 +0200

    [mr1675] Allow mount cast from stealth.

--- src/game/Unit.h
+++ src/game/Unit.h
@@ -1728,7 +1728,7 @@ class MANGOS_DLL_SPEC Unit : public WorldObject
         {
             ShapeshiftForm form = GetShapeshiftForm();
             return form != FORM_NONE && form != FORM_BATTLESTANCE && form != FORM_BERSERKERSTANCE && form != FORM_DEFENSIVESTANCE &&
-                form != FORM_SHADOW;
+                form != FORM_SHADOW && form != FORM_STEALTH;
         }
 
         virtual uint32 GetModelForForm(SpellShapeshiftFormEntry const* ssEntry) const;
