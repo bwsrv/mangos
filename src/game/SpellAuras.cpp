6f24b03a47d72cc2fcb2d61f48900acdf6d92318
Reamer <philipp.dallig@web.de>
2012-02-13 14:12:39 +0100

    [mr1725] Fix for Dot Ticks if Caster is dead.
    Signed-off-by: Reamer <philipp.dallig@web.de>

--- src/game/SpellAuras.cpp
+++ src/game/SpellAuras.cpp
@@ -8402,7 +8402,7 @@ void Aura::PeriodicTick()
             if (!pCaster)
                 return;
 
-            if (!pCaster->IsInWorld() || !pCaster->isAlive())
+            if (!pCaster->IsInWorld())
                 return;
 
             if (spellProto->Effect[GetEffIndex()] == SPELL_EFFECT_PERSISTENT_AREA_AURA &&
