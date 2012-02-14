8de933e621262b6f645c37aed0ac3d20f0c4ca31
rsa <kris13@bk.ru>
2012-02-14 18:33:58 +0400

    [mr1728] more correct variant of mr1726 (fix deleting last aura in holder by mechanic), thx to michalpolko

--- src/game/Unit.cpp
+++ src/game/Unit.cpp
@@ -12331,7 +12331,12 @@ void Unit::RemoveAurasAtMechanicImmunity(uint32 mechMask, uint32 exceptSpellId, 
 
                         Aura* aura = iter->second->GetAuraByEffectIndex(SpellEffectIndex(i));
                         if (aura && !aura->IsDeleted())
-                            aurasToRemove.insert(aura);
+                        {
+                            if (!aura->IsLastAuraOnHolder())
+                                aurasToRemove.insert(aura);
+                            else
+                                spellsToRemove.insert(iter->second->GetId());
+                        }
                     }
                 }
 
