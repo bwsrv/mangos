1acb5fd7c8c2396f9c7b1eacb9bc09ad75c22b9e
boxa <vb.i@mail.ru>
2012-02-19 02:54:31 +0400

    [mr1741] Restore broken rogue talent Shadow Dance. spell #51713

--- src/game/SpellAuras.cpp
+++ src/game/SpellAuras.cpp
@@ -4023,6 +4023,7 @@ void Aura::HandleAuraModShapeshift(bool apply, bool Real)
         switch (form)
         {
             case FORM_CAT:
+            case FORM_SHADOW_DANCE:
                 PowerType = POWER_ENERGY;
                 break;
             case FORM_BEAR:
