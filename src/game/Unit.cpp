ad3fddb2381e8134d984827116ea31eb039679a6
Lillecarl <LilleCarl@gmail.com>
2012-03-13 22:31:35 +0400

    [mr1795] fix improved faerie fire. close #840

--- src/game/Unit.cpp
+++ src/game/Unit.cpp
@@ -7931,7 +7931,6 @@ bool Unit::IsSpellCrit(Unit *pVictim, SpellEntry const *spellProto, SpellSchoolM
                                 }
                             }
                         }
-                        break;
                         // Improved Faerie Fire
                         if (pVictim->HasAura(770) || pVictim->HasAura(16857))
                         {
